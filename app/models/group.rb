b645ae2b83b7c788866ebd7d957421f4ef671a49
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2006-12-31 17:24:00 +0000

    git-svn-id: svn://beonebody.org/onebody/trunk@92 88fc0095-4724-0410-ab6c-c99dba55acc4

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,7 +11,7 @@ class Group < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_format_of :address, :with => /^[a-zA-Z0-9]+$/, :allow_nil => true
   validates_uniqueness_of :address, :allow_nil => true
-  validates_length_of :address, :minimum => 6, :allow_nil => true
+  validates_length_of :address, :minimum => 2, :allow_nil => true
   
   acts_as_photo 'db/photos/groups', PHOTO_SIZES
   
