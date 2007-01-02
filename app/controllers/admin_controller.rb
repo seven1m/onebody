2aff631fde5f978a731904bedafc10aff049f011
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-01-02 00:02:40 +0000

    git-svn-id: svn://beonebody.org/onebody/trunk@95 88fc0095-4724-0410-ab6c-c99dba55acc4

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,8 +10,9 @@ class AdminController < ApplicationController
       if ['Picture', 'Family', 'Groups', 'People', 'Recipe'].include? model_name
         model = eval(model_name)
         id = path.split('/').last.gsub(/\.jpg$/i, '').to_i
-        record = model.find(id)
-        @items << PhotoFile.new(path, record, File::Stat.new(path).mtime)
+        if record = model.find(id) rescue nil
+          @items << PhotoFile.new(path, record, File::Stat.new(path).mtime)
+        end
       end
     end
     @items << Person.find(:all, :limit => RECORD_LIMIT, :order => 'updated_at desc')
@@ -25,6 +26,7 @@ class AdminController < ApplicationController
     @items << Tag.find(:all, :limit => RECORD_LIMIT, :order => 'updated_at desc')
     @items.flatten!
     @items.sort! { |a, b| b.updated_at.strftime('%Y%m%d%H%M%S') <=> a.updated_at.strftime('%Y%m%d%H%M%S') }
+    @items = @items[0..100]
   end
   
   def updates
