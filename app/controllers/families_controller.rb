59f8610574a01828d764f1acff3efb5354e213e4
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-01-10 11:20:55 +0000

    git-svn-id: svn://beonebody.org/onebody/trunk@120 88fc0095-4724-0410-ab6c-c99dba55acc4

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -11,8 +11,9 @@ class FamiliesController < ApplicationController
   def edit
     @family = params[:id] ? Family.find(params[:id]) : @logged_in.family
     unless @logged_in.can_edit? @family.people.first
-      render :text => 'Sorry. You may not change this family photo because you are not an adult according to our records. (If we're wrong, let us know!)', :layout => true
+      render :text => 'Sorry. You may not change this family photo because you are not an adult according to our records. (If we are wrong, let us know!)', :layout => true
       return
+    end
     if request.post?
       if params[:photo_url] and params[:photo_url].length > 7
         @family.photo = params[:photo_url]
