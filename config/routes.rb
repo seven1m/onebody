544a1a649a44b2ca6f104385a9b53f8063ba895d
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-04-03 01:50:29 +0000

    git-svn-id: svn://beonebody.org/onebody/trunk@174 88fc0095-4724-0410-ab6c-c99dba55acc4

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,6 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
+  
+  map.connect ':controller/photo/:id', :action => 'photo', :requirements => { :id => /.*/ }
 end
