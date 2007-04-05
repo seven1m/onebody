62f402199afca793b84e954b1b060c3f034db368
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-04-05 20:11:25 +0000

    git-svn-id: svn://beonebody.org/onebody/trunk@201 88fc0095-4724-0410-ab6c-c99dba55acc4

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -204,7 +204,7 @@ class Notifier < ActionMailer::Base
                     :body => body,
                     :parent => message
                   )
-                  if message.errors.on_base != 'already saved' and message.errors.on_base != 'autoreply'
+                  if message.errors.any? and message.errors.on_base != 'already saved' and message.errors.on_base != 'autoreply'
                     # notify user there were some errors
                     Notifier.deliver_simple_message(email.from, 'Message Error', "Your message with subject \"#{email.subject}\" was not delivered.\n\nSorry for the inconvenience, but the #{SITE_TITLE} site had trouble saving the message (#{message.errors.full_messages.join('; ')}). You may post your message directly from the site after signing into #{SITE_URL}. If you continue to have trouble, please contact #{TECH_SUPPORT_CONTACT}.")
                   end
