85de82f686968ccfd5743909df523da3f35c85d8
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-07-17 18:43:34 +0000

    Moved database.yml and settings.rb to example files to prevent overwrite when using subversion checkouts.
    git-svn-id: svn://beonebody.org/onebody/trunk@284 88fc0095-4724-0410-ab6c-c99dba55acc4

--- config/environment.rb
+++ config/environment.rb
@@ -79,6 +79,10 @@ FLAG_AGES = { # flag interactions between adults and youth
   :adult => 30..120
 }
 
+SITE_URL = "http://#{SITE_SIMPLE_URL}/"
+VISITOR_URL = "http://#{VISITOR_SIMPLE_URL}/"
+NEWS_URL = "http://#{NEWS_SIMPLE_URL}/"
+
 SITE_MAIL_DESCRIPTION = 'One user can send a message to another via this site. The system is monitored for abuse and allows people to contact you without getting your email address. We recommend you leave your email address private to prevent unsolicited email.'
 WALL_DESCRIPTION = 'The Wall is a place for people to post friendly messages for everyone to see. The messages are not private (except that you must be signed in). Think of it like a guestbook.'
 
