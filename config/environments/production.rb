5abb614eb80193d52b19f95961385a8d03a7b4a0
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-12-17 17:39:53 +0000

    updated production.rb for Rails 2.0.2
    git-svn-id: svn://beonebody.org/onebody/trunk@421 88fc0095-4724-0410-ab6c-c99dba55acc4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+ActionView::Base.cache_template_loading              = false
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
