973bc7e86ae3396d6d7fdf55beec84a1b0eba235
tim <tim@88fc0095-4724-0410-ab6c-c99dba55acc4>
2007-12-19 21:35:56 +0000

    fixed theme acquisition for OneBody instances
    git-svn-id: svn://beonebody.org/onebody/trunk@430 88fc0095-4724-0410-ab6c-c99dba55acc4

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -25,7 +25,8 @@ class Setting < ActiveRecord::Base
   def self.update_template_view_paths
     if SETTINGS['appearance'] and SETTINGS['appearance']['theme']
       ActionController::Base.view_paths.delete_if { |p| p =~ /themes/ }
-      ActionController::Base.view_paths.unshift File.join(RAILS_ROOT, 'themes', SETTINGS['appearance']['theme'])
+      root = defined?(APP_ROOT) ? APP_ROOT : RAILS_ROOT
+      ActionController::Base.view_paths.unshift File.join(root, 'themes', SETTINGS['appearance']['theme'])
     end
   end
 end
