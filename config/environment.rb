fd4224a1bf07bd843c0a04b0c74cba46a19e4d3a
Tim Morgan <tim@timmorgan.org>
2008-03-31 01:31:00 -0500

    Disabled caching, cutting memory usage in half.

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ Rails::Initializer.run do |config|
   config.database_configuration_file = File.expand_path(File.join(File.dirname(__FILE__), 'database.yml'))
 end
 
+ActionController::Base.perform_caching = false
+
 PHONE_HOME_FOR_VERSION_INFO = true unless defined? PHONE_HOME_FOR_VERSION_INFO
 DB_PHOTO_PATH = File.join(RAILS_ROOT, 'db/photos') unless defined? DB_PHOTO_PATH
 DB_PUBLICATIONS_PATH = File.join(RAILS_ROOT, 'db/publications') unless defined? DB_PUBLICATIONS_PATH
--- onebody.rb
+++
@@ -1,4 +0,0 @@
-# When OneBody is installed as a RubyGem, this file is used to
-# load up the boot file from the gem.
-
-require File.join(File.dirname(__FILE__), 'config/boot')
