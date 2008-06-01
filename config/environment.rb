03ddee121653d100985213f70f6ae5e532f9a0e2
Tim Morgan <tim@timmorgan.org>
2008-05-31 21:25:34 -0700

    Removed frozen Rails. Now relies on Rails 2.1 gem.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
+RAILS_GEM_VERSION = '2.1' unless defined? RAILS_GEM_VERSION
+
 Rails::Initializer.run do |config|
   config.action_controller.session_store = :active_record_store
   config.log_path = File.join(File.dirname(__FILE__), "../log/#{RAILS_ENV}.log")
@@ -15,4 +17,5 @@ ActionController::Base.perform_caching = false
 PHONE_HOME_FOR_VERSION_INFO = true unless defined? PHONE_HOME_FOR_VERSION_INFO
 DB_PHOTO_PATH = File.join(RAILS_ROOT, 'db/photos') unless defined? DB_PHOTO_PATH
 DB_PUBLICATIONS_PATH = File.join(RAILS_ROOT, 'db/publications') unless defined? DB_PUBLICATIONS_PATH
+DB_ATTACHMENTS_PATH = File.join(RAILS_ROOT, 'db/attachments') unless defined? DB_ATTACHMENTS_PATH
 DB_TASKS_PATH = File.join(RAILS_ROOT, 'db/tasks') unless defined? DB_TASKS_PATH
--- vendor/rails/railties/lib/rails_generator/generators/components/observer/templates/unit_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class <%= class_name %>ObserverTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
