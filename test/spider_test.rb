86e60293010e3ccfa9c87bb521ec7f36a3c017d5
Tim Morgan <tim@timmorgan.org>
2008-06-11 20:42:49 -0500

    Removed Spider test from integration tests. Will have to remember to run manually periodically.

---
+++ test/spider_test.rb
@@ -0,0 +1,16 @@
+require "#{File.dirname(__FILE__)}/test_helper"
+
+class SpiderTest < ActionController::IntegrationTest
+  include Caboose::SpiderIntegrator
+
+  def test_spider
+    #assert true # disable for now
+    sign_in_as people(:peter)
+    spider(
+      @response.body,
+      '/directory/browse',
+      :verbose => true,
+      :ignore_urls => %w(/account/sign_out /account/sign_in /events /directory/directory_to_pdf)
+    ) # TODO: ignoring /events url for now due to unknown error
+  end
+end
