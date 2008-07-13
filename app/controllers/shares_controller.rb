f45669295adf1b5a57c061f983c6acca87aea2cc
Tim Morgan <tim@timmorgan.org>
2008-07-13 00:05:29 -0500

    Improved Shares/More opening page.

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,7 +1,10 @@
 class SharesController < ApplicationController
   
   def index
-    @publications = Publication.all(:order => 'created_at desc')
+    @pictures = Picture.all(:limit => 10, :order => 'created_at desc')
+    @verses = Verse.all(:limit => 5, :order => 'created_at desc')
+    @recipes = Recipe.all(:limit => 5, :order => 'created_at desc')
+    @publications = Publication.all(:limit => 2, :order => 'created_at desc')
   end
   
 end
