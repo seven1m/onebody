c942c267d0c119567588f5f7c9658b9fb41e6e3a
Tim Morgan <tim@timmorgan.org>
2008-07-14 23:58:24 -0500

    Removed remaining, unused plugins.

--- vendor/plugins/day_questions/init.rb
+++
@@ -1,1 +0,0 @@
-require 'day_questions'
--- vendor/plugins/day_questions/test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'test/unit'
-require 'rubygems'
-require 'active_support'
-
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-require File.dirname(__FILE__) + '/../init'
