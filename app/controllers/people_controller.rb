e396c3ee7706a0effac22147498b9866bf078f33
Tim Morgan <tim@timmorgan.org>
2008-07-15 22:29:47 -0500

    Added auto expiring action caching.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 class PeopleController < ApplicationController
 
-  caches_action :show, :cache_path => Proc.new { |c| "/users/#{Person.logged_in.id}/people/#{c.params[:id]}" }
+  caches_action :show, :for => 1.hour, :cache_path => Proc.new { |c| "people/#{c.params[:id]}_for_#{Person.logged_in.id}" }
   cache_sweeper :person_sweeper, :family_sweeper, :only => %w(create update destroy)
 
   def index
