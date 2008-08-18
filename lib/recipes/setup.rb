c4fb08d8cb3a4820dd57af438f906c061078817d
Tim Morgan <tim@timmorgan.org>
2008-08-17 21:16:36 -0500

    Clean up setup cap recipe.

--- lib/recipes/setup.rb
+++ lib/recipes/setup.rb
@@ -19,41 +19,22 @@ namespace :deploy do
       run "mkdir -p #{shared_path}/themes"
     end
     after 'deploy:setup', 'deploy:shared:setup'
-
-    desc 'Create MySQL database and grant user privileges'
-    task :mysql, :roles => :db do
-      run "mysql -u root -e \"create database onebody; grant all on onebody.* to onebody@localhost identified by '#{get_db_password}'\""
-      yml = render_erb_template(File.dirname(__FILE__) + '/templates/database.yml')
-      put yml, "#{shared_path}/config/database.yml"
-    end
-    after 'deploy:shared:setup', 'deploy:shared:mysql'
     
-    desc 'Point certain OneBody globals to the shared path'
-    task :point_db_dirs do
-      rb = render_erb_template(File.dirname(__FILE__) + '/templates/links.rb')
-      put rb, "#{release_path}/config/initializers/links.rb"
-    end
-    after 'deploy:update_code', 'deploy:shared:point_db_dirs'
-    
-    desc 'Copy public files to the shared public path'
-    task :update_public_files do
-      run "cp -r #{release_path}/public/* #{shared_path}/public/"
-    end
-    after 'deploy:update_code', 'deploy:shared:update_public_files'
-
-    desc 'Symlink certain files to the shared path'
-    task :create_symlinks do
-      run "ln -sf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    end
-    after 'deploy:shared:update_public_files', 'deploy:shared:create_symlinks'
-    
-    desc 'Install/update gem dependencies'
-    task :update_dependencies do
-      run "cd #{release_path} && sudo rake gems:install"
-    end
-    after 'deploy:shared:create_symlinks', 'deploy:shared:update_dependencies'
-
+  end
+  
+  task :after_setup do
+    run "cd #{release_path}"
+    sudo "rake gems:install"
+    run "mysql -u root -e \"create database onebody; grant all on onebody.* to onebody@localhost identified by '#{get_db_password}'\""
+    yml = render_erb_template(File.dirname(__FILE__) + '/templates/database.yml')
+    put yml, "#{shared_path}/config/database.yml"
   end
 
+end
 
+task :after_deploy do
+  rb = render_erb_template(File.dirname(__FILE__) + '/templates/links.rb')
+  put rb, "#{release_path}/config/initializers/links.rb"
+  run "cp -r #{release_path}/public/* #{shared_path}/public/"
+  run "ln -sf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 end
