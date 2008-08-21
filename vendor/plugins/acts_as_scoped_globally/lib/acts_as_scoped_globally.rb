2019ea46e39880bd72d9a3bcc814cd05ecc1891d
Tim Morgan <tim@timmorgan.org>
2008-08-20 21:52:25 -0500

    Allow select/update while global scope is disabled.

--- vendor/plugins/acts_as_scoped_globally/lib/acts_as_scoped_globally.rb
+++ vendor/plugins/acts_as_scoped_globally/lib/acts_as_scoped_globally.rb
@@ -14,39 +14,53 @@ module Foo
             attr_protected '#{foreign_key}'
             
             class << self
-              alias_method :rails_original_find_by_sql, :find_by_sql 
-              def find_by_sql(sql) 
-                sql = sanitize_sql(sql)
-                unless sql.index('#{foreign_key} =') # already present (probably from current_scoped_methods)
-                  unless sql.gsub!(/from\\s+(["`]?[a-z_]+["`]?)\\s+((inner|left\\s+outer)\\s+join\\s+.+)?where/i, "\\\\0 \\\\1.#{foreign_key} = " + #{value}.to_s + " and") or 
-                    sql.gsub!(/from\\s+(["`]?[a-z_]+["`]?)\\s+((inner|left\\s+outer)\\s+join\\s+.+)?/i, "\\\\0where \\\\1.#{foreign_key} = " + #{value}.to_s + ' ') 
-                    raise "Error inserting site selection condition in sql: " + sql
+              def find_by_sql_with_global_scope(sql) 
+                unless disable_global_scope
+                  sql = sanitize_sql(sql)
+                  unless sql.index('#{foreign_key} =') # already present (probably from current_scoped_methods)
+                    unless sql.gsub!(/from\\s+(["`]?[a-z_]+["`]?)\\s+((inner|left\\s+outer)\\s+join\\s+.+)?where/i, "\\\\0 \\\\1.#{foreign_key} = " + #{value}.to_s + " and") or 
+                      sql.gsub!(/from\\s+(["`]?[a-z_]+["`]?)\\s+((inner|left\\s+outer)\\s+join\\s+.+)?/i, "\\\\0where \\\\1.#{foreign_key} = " + #{value}.to_s + ' ') 
+                      raise "Error inserting site selection condition in sql: " + sql
+                    end
                   end
                 end
-	              rails_original_find_by_sql(sql)
+	              find_by_sql_without_global_scope(sql)
 	            end
+	            alias_method_chain :find_by_sql, :global_scope
               
-              alias_method :rails_original_current_scoped_methods, :current_scoped_methods
-              def current_scoped_methods
-                m = rails_original_current_scoped_methods || {}
-                m[:find] ||= {}
-                m[:find][:conditions] ||= ''
-                unless m[:find][:conditions].index('#{foreign_key} =') or
-                  m[:find][:conditions].gsub! /(#{foreign_key} = )\d+/, '\\1' + #{value}.to_s
-                  if m[:find][:conditions].any?
-                    m[:find][:conditions] += ' and'
+              def current_scoped_methods_with_global_scope
+                m = current_scoped_methods_without_global_scope || {}
+                unless disable_global_scope
+                  m[:find] ||= {}
+                  m[:find][:conditions] ||= ''
+                  unless m[:find][:conditions].index('#{foreign_key} =') or
+                    m[:find][:conditions].gsub! /(#{foreign_key} = )\d+/, '\\1' + #{value}.to_s
+                    if m[:find][:conditions].any?
+                      m[:find][:conditions] += ' and'
+                    end
+                    m[:find][:conditions] += ' `#{table_name}`.#{foreign_key} = ' + #{value}.to_s
                   end
-                  m[:find][:conditions] += ' `#{table_name}`.#{foreign_key} = ' + #{value}.to_s
                 end
                 return m
               end
+              alias_method_chain :current_scoped_methods, :global_scope
+              
+              def without_global_scope
+                self.disable_global_scope = true
+                yield
+                self.disable_global_scope = nil
+              end
+              
+              attr_accessor :disable_global_scope
             end
 
-            alias_method :rails_original_create, :create
-            def create
-              self.#{foreign_key} ||= #{value}
-              rails_original_create
+            def create_with_global_scope
+              unless self.class.disable_global_scope
+                self.#{foreign_key} ||= #{value}
+              end
+              create_without_global_scope
             end
+            alias_method_chain :create, :global_scope
           END
         end
       end
