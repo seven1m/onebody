f56edd8c1a2981a818510230e39c86cc7adef73b
Tim Morgan <tim@timmorgan.org>
2008-09-05 08:14:00 -0500

    Use connection.adapter_name for determining database type

--- config/initializers/misc.rb
+++ config/initializers/misc.rb
@@ -5,7 +5,7 @@ end
 PEOPLE_ATTRIBUTES_SHOWABLE_ON_HOMEPAGE = %w(website service_name service_description service_phone service_email service_website activities interests music tv_shows movies books quotes about testimony )
 
 begin
-  SQLITE = Setting.connection.class.name =~ /sqlite/i rescue false
+  SQLITE = Setting.connection.adapter_name == 'SQLite' rescue false
 rescue
   SQLITE = OneBodyInfo.new.database_yaml['production']['adapter'] == 'sqlite3'
 end
