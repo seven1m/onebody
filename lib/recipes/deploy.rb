bb541df8dea85c60da61e1fa2452c0a9c7b75d6b
Tim Morgan <tim@timmorgan.org>
2008-09-19 08:14:00 -0500

    Only copy logo files in cap recipe if source exists.

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -1,7 +1,7 @@
 namespace :deploy do
   
   task :copy_logos do
-    run "cp #{previous_release}/public/images/logo* #{current_release}/public/images/"
+    run "if [ -e #{previous_release}/public/images/logo* ]; then cp #{previous_release}/public/images/logo* #{current_release}/public/images/; fi"
   end
   after 'deploy:update_code', 'deploy:copy_logos'
 
