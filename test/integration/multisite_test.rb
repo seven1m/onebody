9bd2bf128d010022f1d3b785e1a3a02fd3573347
Tim Morgan <tim@timmorgan.org>
2008-09-19 08:14:00 -0500

    Fix multisite test breakage due to changes in use of flash.

--- test/integration/multisite_test.rb
+++ test/integration/multisite_test.rb
@@ -12,15 +12,14 @@ class MultisiteTest < ActionController::IntegrationTest
   
   def test_login
     site! 'site1'
-    sign_in_as people(:jim)
     post_sign_in_form 'tom@example.com'
     assert_response :success
-    assert flash[:warning] =~ /email address cannot be found/
+    assert_select 'body', /email address cannot be found/
     site! 'site2'
     sign_in_as people(:tom)
     post_sign_in_form 'jim@example.com'
     assert_response :success
-    assert flash[:warning] =~ /email address cannot be found/
+    assert_select 'body', /email address cannot be found/
   end
   
   def test_browse
