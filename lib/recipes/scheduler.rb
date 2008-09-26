a5d69023368792f84797a7056a8b42c609bb04d7
Tim Morgan <tim@timmorgan.org>
2008-09-26 17:38:06 -0500

    Make cap onebody:scheduler:stop command work on multiple remote hosts.

--- lib/recipes/scheduler.rb
+++ lib/recipes/scheduler.rb
@@ -10,10 +10,7 @@ namespace :deploy do
     desc 'Stop the Scheduler daemon.'
     task :stop, :roles => :web do
       # stop any running instance in case we forgot to do so before changing the symlink
-      run_and_return("ps aux | grep '[s]cript/scheduler start production'").split("\n").each do |match|
-        pid = match.split[1]
-        run "kill -HUP #{pid}"
-      end
+      run "ps aux | ruby -e \"STDIN.read.select { |p| p =~ /script.scheduler start production/ }.each { |p| Process.kill('HUP', p.split[1].to_i) }\"" rescue nil
     end
     
     desc 'Restart the Scheduler daemon.'
