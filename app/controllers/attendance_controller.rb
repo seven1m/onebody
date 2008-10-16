c69524bdea3dad019a90359fb254a037d5613231
Tim Morgan <tim@timmorgan.org>
2008-10-16 08:14:00 -0500

    Fix Attendance tracking breakage due to UTC time storage. Add tests.

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -4,7 +4,7 @@ class AttendanceController < ApplicationController
     @group = Group.find(params[:group_id])
     if @group.admin?(@logged_in)
       @attended_at = Date.parse(params[:attended_at])
-      @records = @group.get_people_attendance_records_for_date(@attended_at)
+      @records = @group.get_people_attendance_records_for_date(Time.zone.local_to_utc(@attended_at.to_time))
     else
       render :text => 'You are not authorized to view attendance for this group.', :layout => true, :status => 401
     end
@@ -29,7 +29,7 @@ class AttendanceController < ApplicationController
     @group = Group.find(params[:group_id])
     @attended_at = Date.parse(params[:attended_at])
     if @group.admin?(@logged_in)
-      @group.attendance_records.find_all_by_attended_at(@attended_at.to_time).each { |r| r.destroy }
+      @group.attendance_records.find_all_by_attended_at(Time.zone.local_to_utc(@attended_at.to_time)).each { |r| r.destroy }
       params[:ids].to_a.each do |id|
         if person = Person.find_by_id(id)
           @group.attendance_records.create!(:person_id => person.id, :attended_at => @attended_at.strftime('%Y-%m-%d'))
--- test/functional/attendance_controller_test.rb
+++ test/functional/attendance_controller_test.rb
@@ -1,8 +1,28 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class AttendanceControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  def setup
+    @person = Person.forge
+    @group = Group.forge(:creator_id => @person.id, :category => 'Small Groups')
+    @group.memberships.create(:person => @person, :admin => true)
   end
+  
+  should "store and retrieve attendance records based on date" do
+    post :batch, {:attended_at => '2009-12-01', :group_id => @group.id, :ids => [@person.id], :format => 'js'}, {:logged_in_id => @person.id}
+    get  :index, {:attended_at => '2009-12-01', :group_id => @group.id, :format => 'js'}, {:logged_in_id => @person.id}
+    assert_equal 1, assigns(:records).length
+    person, attended = assigns(:records).first
+    assert attended
+  end
+  
+  should "overwrite existing records" do
+    post :batch, {:attended_at => '2009-12-01', :group_id => @group.id, :ids => [@person.id], :format => 'js'}, {:logged_in_id => @person.id}
+    post :batch, {:attended_at => '2009-12-01', :group_id => @group.id, :ids => [], :format => 'js'}, {:logged_in_id => @person.id}
+    get  :index, {:attended_at => '2009-12-01', :group_id => @group.id, :format => 'js'}, {:logged_in_id => @person.id}
+    assert_equal 1, assigns(:records).length
+    person, attended = assigns(:records).first
+    assert !attended
+  end
+  
 end
