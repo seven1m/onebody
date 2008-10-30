832f1fcd7f3bf7202203b03a14cb7d86e4a2be44
Tim Morgan <tim@timmorgan.org>
2008-10-30 08:14:00 -0500

    Change fixture test data just a bit.

--- test/unit/update_test.rb
+++ test/unit/update_test.rb
@@ -5,22 +5,21 @@ class UpdateTest < Test::Unit::TestCase
 
   should "update a person" do
     Person.logged_in = people(:tim)
-    assert updates(:update_tim).do!
-    people(:tim).reload
+    assert updates(:update_jeremy).do!
     %w(first_name last_name suffix gender mobile_phone work_phone fax).each do |attribute|
-      assert_equal updates(:update_tim)[attribute], people(:tim)[attribute]
+      assert_equal updates(:update_jeremy)[attribute], people(:jeremy)[attribute]
     end
     %w(birthday anniversary).each do |attribute|
-      assert_equal updates(:update_tim)[attribute].to_s, people(:tim)[attribute].to_s
+      assert_equal updates(:update_jeremy)[attribute].to_s, people(:jeremy)[attribute].to_s
     end
     %w(home_phone address1 address2 city state zip family_name family_last_name).each do |attribute|
-      assert_equal updates(:update_tim)[attribute], people(:tim).family[attribute.gsub(/^family_/, '')]
+      assert_equal updates(:update_jeremy)[attribute], people(:jeremy).family[attribute.sub(/^family_/, '')]
     end
   end
   
   should "list only the changes" do
     # better than nothing
-    assert_equal 7, updates(:update_tim).changes.keys.length
+    assert_equal 9, updates(:update_jeremy).changes.keys.length
   end
   
 end
