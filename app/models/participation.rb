b02c30e121971c47b1008782cb2a6f43dd8faf59
Kris Martin <kris@drogba.local>
2008-11-18 23:54:07 +1100

    Add site_id to Participation models.

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,7 @@
 class Participation < ActiveRecord::Base
   belongs_to :person
   belongs_to :participation_category
+  belongs_to :site
+  
+  acts_as_scoped_globally 'site_id', "(Site.current ? Site.current.id : 'site-not-set')"
 end
--- db/migrate/20081108130434_create_participations.rb
+++ db/migrate/20081108130434_create_participations.rb
@@ -1,24 +1,16 @@
 class CreateParticipations < ActiveRecord::Migration
   def self.up
     create_table :participation_categories do |t|
-      t.string :name, :null => false
-      t.text   :description, :null => true
+      t.string  :name, :null => false
+      t.text    :description, :null => true
+      t.integer :site_id
     end
     
-    # Populates the participation_categories table with some sample data.
-    # TODO - to be removed when participation categories management UI are completed.
-    ParticipationCategory.create(:name => "Choir")
-    ParticipationCategory.create(:name => "Liturgist")
-    ParticipationCategory.create(:name => "Offering")
-    ParticipationCategory.create(:name => "Sound System")
-    ParticipationCategory.create(:name => "Sunday School")
-    ParticipationCategory.create(:name => "Usher")
-    ParticipationCategory.create(:name => "Website")
-
     create_table :participations do |t|
       t.integer :person_id, :null => false
       t.integer :participation_category_id, :null => false
       t.string  :status, :null => false, :default => 'current' # current | pending | completed
+      t.integer :site_id
       t.timestamps
     end
   end
