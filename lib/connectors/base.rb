8c55f4779e7f1ef3d192eaed2b77cb4788220397
Tim Morgan <tim@timmorgan.org>
2008-12-26 11:13:20 -0600

    remove connectors directory

--- lib/connectors/base.rb
+++
@@ -1,7 +0,0 @@
-# This class doesn't do much yet, but it might in the future.
-
-# See example.rb for more help in creating your own connector.
-
-class ExternalDataConnector
-  def logger; RAILS_DEFAULT_LOGGER; end
-end
