9f16930cf8547728f524405d598418fd3b7816f6
Tim Morgan <tim@timmorgan.org>
2009-02-07 23:55:05 -0600

    Fix bug in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,5 +16,3 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
-
-ActionView::Base.cache_template_loading              = true
