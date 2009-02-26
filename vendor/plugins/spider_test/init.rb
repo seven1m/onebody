81fb5a045e7b57362efe03410138c39f52d0a7b1
Tim Morgan <tim@timmorgan.org>
2009-02-26 08:14:00 -0600

    Remove old spider test.

--- vendor/plugins/spider_test/init.rb
+++
@@ -1,1 +0,0 @@
-require 'caboose/spider_integrator'
--- vendor/plugins/spider_test/install.rb
+++
@@ -1,2 +0,0 @@
-File.open(File.dirname(__FILE__)+'/README') { |f| puts f.read.split(/=+/)[0..1].join }
-puts "Please view the README for more instructions."
