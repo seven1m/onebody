3e6306d305fc82fdc46be004d699e492a7fbf711
Tim Morgan <tim@timmorgan.org>
2009-02-27 17:56:04 -0600

    Add stale session cleanup to schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,10 @@ every 1.hour, :at => 19 do
   runner 'Group.update_memberships; NewsItem.update_from_feed; LogItem.flag_suspicious_activity'
 end
 
+every 1.day, :at => '2:49 am' do
+  runner "CGI::Session::ActiveRecordStore::Session.delete_all(['updated_at < ?', 1.day.ago.utc])"
+end
+
 if File.exist?('config/email.yml')
   every 1.minute do
     settings = YAML::load_file('config/email.yml')[@environment.to_s]['pop']
