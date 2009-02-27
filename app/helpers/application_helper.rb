c4d449e2d09b109a5623752766613b7457e417ab
Tim Morgan <tim@timmorgan.org>
2009-02-26 22:00:16 -0600

    Link directly to logged in user profile page in nav bar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,7 +35,7 @@ module ApplicationHelper
     if Setting.get(:features, :content_management_system)
       html << "<li>#{tab_link 'Home', '/', params[:controller] == 'pages' && @page && @page.home?}</li>"
     end
-    html << "<li>#{tab_link 'Profile', people_path, params[:controller] == 'people' && me?}</li>"
+    html << "<li>#{tab_link 'Profile', @logged_in || people_path, params[:controller] == 'people' && me?}</li>"
     html << "<li>#{tab_link 'Directory', new_search_path, %w(searches printable_directories).include?(params[:controller])}</li>"
     if Setting.get(:features, :groups) and (Site.current.max_groups.nil? or Site.current.max_groups > 0)
       html << "<li>#{ tab_link 'Groups', groups_path, params[:controller] == 'groups'}</li>"
