e5fee281209e3370363b6e990901462dce1ba0bb
Tim Morgan <tim@timmorgan.org>
2009-03-02 19:47:38 -0600

    Copy assets and custom themes from previous release on deploy.

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -4,5 +4,15 @@ namespace :deploy do
     run "if [ -e #{previous_release}/public/images ]; then cp #{previous_release}/public/images/logo* #{current_release}/public/images/; fi"
   end
   after 'deploy:update_code', 'deploy:copy_logos'
+  
+  task :copy_assets do
+    run "if [ -e #{previous_release}/public/assets ]; then cp #{previous_release}/public/assets/* #{current_release}/public/assets/; fi"
+  end
+  after 'deploy:update_code', 'deploy:copy_assets'
 
+  task :copy_custom_themes do
+    run "if [ -e #{previous_release}/themes/custom ]; then cp #{previous_release}/themes/custom/* #{current_release}/themes/custom/; fi"
+  end
+  after 'deploy:update_code', 'deploy:copy_custom_themes'
+  
 end
