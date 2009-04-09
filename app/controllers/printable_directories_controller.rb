6ba4cc5c6f2431a4ba19d8fc0154f8af68de12bb
Tim Morgan <tim@timmorgan.org>
2009-04-08 23:36:15 -0500

    Remove that leading slash.

--- app/controllers/printable_directories_controller.rb
+++ app/controllers/printable_directories_controller.rb
@@ -7,7 +7,7 @@ class PrintableDirectoriesController < ApplicationController
   
   def create
     if session[:directory_pdf_job].nil? or session[:directory_pdf_job] < 1.hour.ago
-      system("#{File.expand_path(Rails.root + '/script/runner')} -e #{Rails.env} \"Site.current = Site.find(#{Site.current.id}); Person.find(#{@logged_in.id}).generate_and_email_directory_pdf(#{params[:with_pictures] ? 'true' : 'false'})\" &")
+      system("#{File.expand_path(Rails.root + 'script/runner')} -e #{Rails.env} \"Site.current = Site.find(#{Site.current.id}); Person.find(#{@logged_in.id}).generate_and_email_directory_pdf(#{params[:with_pictures] ? 'true' : 'false'})\" &")
       session[:directory_pdf_job] = Time.now
     else
       render :text => "It seems a directory was already sent to you a little while ago. Please check your email (and your spam folder just in case).", :layout => true, :status => 401
