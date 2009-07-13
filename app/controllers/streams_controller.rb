17777d687fa2d06ed5ee9261d0774f36d73d20b4
Tim Morgan <tim@timmorgan.org>
2009-07-13 07:17:18 -0500

    Better arrangement of stream page.

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -2,11 +2,6 @@ class StreamsController < ApplicationController
 
   def show
     @stream_items = @logged_in.stream_items(30)
-    @person = @logged_in
-    @family = @person.family
-    @friends = @person.friends.all(:limit => MAX_FRIENDS_ON_PROFILE).select { |p| @logged_in.can_see?(p) }
-    @sidebar_group_people = @person.random_sidebar_group_people.select { |p| @logged_in.can_see?(p) }
-    @family_people = @person.family.visible_people
   end
 
 end
