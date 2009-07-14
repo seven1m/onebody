50aceb43a93c55d27fe61d1484808d8cf6e9ca37
Tim Morgan <tim@timmorgan.org>
2009-07-14 07:23:32 -0500

    Don't need those extra columns on the feed model.

--- db/migrate/20090708025608_create_feeds.rb
+++ db/migrate/20090708025608_create_feeds.rb
@@ -2,8 +2,7 @@ class CreateFeeds < ActiveRecord::Migration
   def self.up
     create_table :feeds do |t|
       t.string :name, :limit => 100
-      t.string :rss_url, :limit => 1000
-      t.string :twitter_username, :facebook_uid, :limit => 25
+      t.string :url, :limit => 1000
       t.timestamps
     end
   end
