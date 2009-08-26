b162e5c2763a62b5955efb5e02b81d66d7e6c006
Tim Morgan <tim@timmorgan.org>
2009-08-26 07:18:17 -0500

    Easier switching of current site in script/console.

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -1,5 +1,14 @@
 # Convenience for script/console so that default site is already set as current site
 if $0 == 'irb'
   Site.current = Site.find(1)
-  puts 'Site.current set to Default Site'
+  puts 'Sites:'
+  puts '  id    name                                                         host'
+  puts '  ----- ------------------------------------------------------------ -----------------------------'
+  puts Site.all.map { |s| "#{s.default? ? '*' : ' '} #{s.id.to_s.ljust 5} #{s.name.ljust(60)[0...60]} #{s.host}" }.join("\n")
+  def use(id)
+    Site.current = Site.find(id)
+    puts "Set Site.current to <#{Site.current.name}>"
+    true
+  end
+  puts 'Type "use ID" to change selected site...'
 end
