d8657545db22f5b292603055149eed3ca7cc6a82
Tim Morgan <tim@timmorgan.org>
2009-08-27 07:10:19 -0500

    Additional indexes.

--- db/migrate/20090827144622_add_indexes_on_site_id.rb
+++ db/migrate/20090827144622_add_indexes_on_site_id.rb
@@ -2,10 +2,12 @@ class AddIndexesOnSiteId < ActiveRecord::Migration
   def self.up
     add_index "people", ["site_id"], :name => "index_site_id_on_people"
     add_index "groups", ["site_id"], :name => "index_site_id_on_groups"
+    add_index "admins", ["site_id"], :name => "index_site_id_on_admins"
   end
 
   def self.down
     remove_index :name => "index_site_id_on_people"
     remove_index :name => "index_site_id_on_groups"
+    remove_index :name => "index_site_id_on_admins"
   end
 end
---
+++ db/migrate/20090827151534_add_index_admin_id_on_people.rb
@@ -0,0 +1,9 @@
+class AddIndexAdminIdOnPeople < ActiveRecord::Migration
+  def self.up
+    add_index "people", ["admin_id"], :name => "index_admin_id_on_people"
+  end
+
+  def self.down
+    remove_index :name => "index_admin_id_on_people"
+  end
+end
