3a89dcda8b1d7be4eb6ce75528223ba08845f823
Tim Morgan <tim@timmorgan.org>
2009-09-17 00:39:47 -0500

    Don't barf in test env if a yml file is missing.

--- config/initializers/_settings.rb
+++ config/initializers/_settings.rb
@@ -3,8 +3,11 @@ if RAILS_ENV == 'test' and ActiveRecord::Base.connected?
   %w(settings sites).each do |file|
     model = eval(file.singularize.classify)
     next unless model.table_exists?
-    YAML::load(File.open(File.join(RAILS_ROOT, "test/fixtures/#{file}.yml"))).each do |fixture, values|
-      model.create(values)
+    path = File.join(RAILS_ROOT, "test/fixtures/#{file}.yml")
+    if File.exist?(path)
+      YAML::load(File.open(path)).each do |fixture, values|
+        model.create(values)
+      end
     end
   end
   Site.current = Site.find(1) if Site.table_exists?
