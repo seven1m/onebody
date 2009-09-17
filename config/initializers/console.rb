f1da6c60379fd689fe2f0982dbb261a06d36589a
Tim Morgan <tim@timmorgan.org>
2009-09-17 00:40:10 -0500

    Prevent script/console site menu from wrapping.

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -1,10 +1,9 @@
-# Convenience for script/console so that default site is already set as current site
-if $0 == 'irb'
+if $0 =~ /^irb/
   Site.current = Site.find(1)
   puts 'Sites:'
   puts '  id    name                                     host'
-  puts '  ----- ---------------------------------------- -----------------------------'
-  puts Site.all.map { |s| "#{s.default? ? '*' : ' '} #{s.id.to_s.ljust 5} #{s.name.ljust(40)[0...40]} #{s.host}" }.join("\n")
+  puts '  ----- ---------------------------------------- ------------------------------'
+  puts Site.all.map { |s| "#{s.default? ? '*' : ' '} #{s.id.to_s.ljust 5} #{s.name.ljust(40)[0...40]} #{s.host.ljust(30)[0...30]}" }.join("\n")
   def use(id)
     Site.current = Site.find(id)
     puts "Set Site.current to <#{Site.current.name}>"
