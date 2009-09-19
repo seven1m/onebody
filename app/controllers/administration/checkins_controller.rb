f7186c30390256d307848cd78a1630cdee4dfbda
Tim Morgan <tim@timmorgan.org>
2009-09-19 15:55:33 -0500

    Ability to export barcodes to csv.

--- app/controllers/administration/checkins_controller.rb
+++ app/controllers/administration/checkins_controller.rb
@@ -3,7 +3,13 @@ class Administration::CheckinsController < ApplicationController
   before_filter :only_admins
   
   def show
-    
+    respond_to do |format|
+      format.html
+      format.csv do
+        @families = Family.all(:select => 'id, legacy_id, name, barcode_id', :conditions => "barcode_id is not null and barcode_id != ''", :order => 'name')
+        render :text => @families.to_csv
+      end
+    end
   end
   
 end
