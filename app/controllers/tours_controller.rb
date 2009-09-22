a1c65c8bd6be1ab65e04450a4a058dab6e4cf507
Tim Morgan <tim@timmorgan.org>
2009-09-22 07:15:51 -0500

    Expire cache when tour starts or stops.

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -13,11 +13,13 @@ class ToursController < ApplicationController
 
   def create
     session[:touring] = true
+    expire_fragment(%r{views/people/#{@logged_in.id}_})
     redirect_to stream_path
   end
   
   def destroy
     session[:touring] = false
+    expire_fragment(%r{views/people/#{@logged_in.id}_})
     redirect_back
   end
 
