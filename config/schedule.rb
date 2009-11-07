62571a9a1cc1b04d85c7f530300b1a56cc47f443
Tim Morgan <tim@timmorgan.org>
2009-11-07 15:48:33 -0600

    Update scheduled jobs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,17 +2,20 @@
 
 set :environment, 'production_lite'
 
-every 1.hour, :at => 19 do
-  runner 'Site.each { Group.update_memberships; Feed.import_all; NewsItem.update_from_feed; LogItem.flag_suspicious_activity }'
-end
-
-every 1.day, :at => '2:49 am' do
-  runner "ActionController::Session::ActiveRecordStore::Session.delete_all(['updated_at < ?', 1.day.ago.utc])"
-end
-
 if File.exist?('config/email.yml')
   every 1.minute do
     settings = YAML::load_file('config/email.yml')['production']['pop']
     command "#{Rails.root}/script/inbox -e #{@environment} \"#{settings['host']}\" \"#{settings['username']}\" \"#{settings['password']}\""
   end
+end
+
+every 1.hour, :at => 19 do
+  runner 'Site.each { Feed.import_all; NewsItem.update_from_feed }'
+end
+
+every 1.day, :at => '3:49 am' do
+  runner <<-END
+    ActionController::Session::ActiveRecordStore::Session.delete_all(['updated_at < ?', 1.day.ago.utc])"
+    Site.each { Group.update_memberships; LogItem.flag_suspicious_activity }
+  END
 end
