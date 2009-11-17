16b7a5b2161d445f2a3818a2f28409f5be9114cb
Tim Morgan <tim@timmorgan.org>
2009-11-17 00:21:12 -0600

    Set the default locale to :en.
    You can change the default (without having to commit the change to the repo), by creating a config/locale file (which is ignored by .gitignore) with your local name in it.

--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,7 @@ Rails::Initializer.run do |config|
   config.load_paths << "#{Rails.root}/app/sweepers"
   config.plugin_paths << "#{Rails.root}/plugins"
   config.time_zone = 'UTC'
+  config.i18n.default_locale = File.exist?("#{RAILS_ROOT}/config/locale") ? File.read("#{RAILS_ROOT}/config/locale").strip.to_sym : :en
   # dependencies 
   config.gem 'tobi-liquid',                     :source => 'http://gems.github.com', :lib => 'liquid'
   config.gem 'color'
@@ -37,11 +38,6 @@ Rails::Initializer.run do |config|
   # to install with your gem command (rake gems:install uses just plain 'gem' by default -- you might need to use gem19 or something else):
   # [gemcommand] install highline mini_magick twitter xmpp4r chronic nokogiri builder color transaction-simple
   # [gemcommand] install -s http://gems.github.com tobi-liquid metaskills-pdf-writer mislav-will_paginate seven1m-acts_as_scoped_globally seven1m-has_one_photo seven1m-has_one_file seven1m-campaign_monitor javan-whenever pauldix-feedzirra
-
-  # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
-  # All files from config/locales/*.rb,yml are added automatically.
-  #config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  config.i18n.default_locale = :pt
 end
 
 PHONE_HOME_FOR_VERSION_INFO = true unless defined? PHONE_HOME_FOR_VERSION_INFO
