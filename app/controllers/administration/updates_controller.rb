fa6d7f0cc4991afc549f4d156d08cb27725accdd
Tim Morgan <tim@timmorgan.org>
2009-11-18 07:11:11 -0600

    Mark updates compelte without doing anything.

--- app/controllers/administration/updates_controller.rb
+++ app/controllers/administration/updates_controller.rb
@@ -13,22 +13,27 @@ class Administration::UpdatesController < ApplicationController
   def update
     @update = Update.find(params[:id])
     if params[:complete] == 'true'
-      if params[:update] and params[:update][:child]
-        @update.child = (params[:update][:child] == 'true')
-      elsif @update.birthday.nil?
-        flash[:warning] = 'You must specify whether the person is a Child (under 13) or not to complete this update.'
-        redirect_to administration_updates_path
-        return
-      end
-      if @update.do!
-        @update.update_attribute(:complete, true)
-        if params[:review]
-          redirect_to edit_person_path(@update.person, :anchor => 'basics')
-        else
+      if params[:commit]
+        if params[:update] and params[:update][:child]
+          @update.child = (params[:update][:child] == 'true')
+        elsif @update.birthday.nil?
+          flash[:warning] = 'You must specify whether the person is a Child (under 13) or not to complete this update.'
           redirect_to administration_updates_path
+          return
+        end
+        if @update.do!
+          @update.update_attribute(:complete, true)
+          if params[:review]
+            redirect_to edit_person_path(@update.person, :anchor => 'basics')
+          else
+            redirect_to administration_updates_path
+          end
+        else
+          render :action => 'error', :status => 500
         end
       else
-        render :action => 'error', :status => 500
+        @update.update_attribute(:complete, true)
+        redirect_to administration_updates_path
       end
     else
       @update.update_attribute(:complete, false)
