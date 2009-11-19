2a21b0a6b0c0c401d1d360f1ec946803c5f4c0c7
Tim Morgan <tim@timmorgan.org>
2009-11-19 07:17:49 -0600

    Improved display of changed values in log.

--- app/helpers/administration/log_items_helper.rb
+++ app/helpers/administration/log_items_helper.rb
@@ -4,11 +4,18 @@ module Administration::LogItemsHelper
     send(log_item.loggable_type.underscore + '_path', log_item.loggable_id)
   end
   
-  def log_item_change_value(value)
+  def log_item_change_value(log_item, attribute, value)
+    model = Kernel.const_get(log_item.loggable_type)
     if value.is_a?(Time)
-      value.in_time_zone
+      if model.skip_time_zone_conversion_for_attributes.include?(attribute.to_sym)
+        value
+      else
+        value.in_time_zone
+      end
     elsif value.respond_to?(:name)
       value.name
+    elsif value.nil?
+      'nil'
     else
       value
     end
