3b764ae07ab73238c5dac0ec9b959792d54d5815
Tim Morgan <tim@timmorgan.org>
2009-12-26 22:01:18 -0600

    Sort groupies by name.

--- app/controllers/groupies_controller.rb
+++ app/controllers/groupies_controller.rb
@@ -3,7 +3,7 @@ class GroupiesController < ApplicationController
   def index
     @person = Person.find(params[:person_id])
     if @logged_in.can_see?(@person)
-      @people = @person.sidebar_group_people.select { |p| @logged_in.can_see?(p) }
+      @people = @person.sidebar_group_people.select { |p| @logged_in.can_see?(p) }.sort_by { |p| [p.last_name, p.first_name] }
     else
       render :text => 'Person not found.', :layout => true, :status => 404
     end
