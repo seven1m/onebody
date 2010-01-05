0966dfde07f3bb4da6925553e87822635ec9f5b0
Tim Morgan <tim@timmorgan.org>
2010-01-05 07:17:16 -0600

    Set which attributes are accessible in Album model.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -45,12 +45,14 @@ class AlbumsController < ApplicationController
   end
   
   def create
-    @album = Album.new(params[:album].merge(:person_id => @logged_in.id))
+    @album = Album.new(params[:album])
     if @album.group and !can_add_pictures_to_group?(@album.group)
       @album.errors.add_to_base('Cannot add pictures in this group.')
     end
     if params['remove_owner'] and @logged_in.admin?(:manage_pictures)
       @album.person = nil
+    else
+      @album.person = @logged_in
     end
     if @album.save
       flash[:notice] = 'Album saved.'
