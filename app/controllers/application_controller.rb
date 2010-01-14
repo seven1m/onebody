7d669b1c35cb1986702de6d3c5b37d2969d5c756
Tim Morgan <tim@timmorgan.org>
2010-01-13 23:08:59 -0600

    Optional env variable for specifying site to show in dev mode.
    In a multi-site instance, the following command will start dev mode for
    the specified site and eliminate hostname hackery:
    
    ONEBODY_SITE="My Site" script/server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,9 @@ class ApplicationController < ActionController::Base
   
   private
     def get_site
-      if Setting.get(:features, :multisite)
+      if ENV['ONEBODY_SITE']
+        Site.current = Site.find_by_name_and_active(ENV['ONEBODY_SITE'], true)
+      elsif Setting.get(:features, :multisite)
         Site.current = Site.find_by_host_and_active(request.host, true)
       else
         Site.current = Site.find(1) or raise 'No Default site found.'
