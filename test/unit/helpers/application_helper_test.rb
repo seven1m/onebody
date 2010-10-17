ec49b1eb349cd377da2d320765ae04d6d904b9c8
Tim Morgan <tim@timmorgan.org>
2010-10-16 20:00:15 -0500

    More tests for the white_list_with_removal helper.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -4,17 +4,21 @@ class ApplicationHelperTest < ActionView::TestCase
 
   include WhiteListHelper
 
-  context 'Whitelist' do
+  context 'white_list_with_removal' do
     should 'remove style tags and their content' do
       assert_equal('before after', white_list_with_removal('before <style type="text/css">body { font-size: 12pt; }</style>after'))
     end
-
     should 'remove script tags and their content' do
       assert_equal('before after', white_list_with_removal('before <script type="text/javascript">alert("hi")</script>after'))
     end
-
     should 'remove other illegal tags' do
       assert_equal('before and after', white_list_with_removal('before <bad>and</bad> after'))
+    end
+    should 'allow safe tags' do
+      assert_equal('before <strong>bold</strong> and <em>italic</em> after', white_list_with_removal('before <strong>bold</strong> and <em>italic</em> after'))
+    end
+    should 'be html_safe' do
+      assert white_list_with_removal('<strong>safe</strong>').html_safe
     end
   end
 
