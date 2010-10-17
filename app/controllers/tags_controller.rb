fcf4112e673ae3b9d1e766b8ce67290145159037
Tim Morgan <tim@timmorgan.org>
2010-10-17 10:16:17 -0500

    Fix integer being passed from functional test.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def show
-    @tag = params[:id] =~ /^\d+$/ ? Tag.find(params[:id]) : Tag.find_by_name(params[:id])
+    @tag = params[:id].to_s =~ /^\d+$/ ? Tag.find(params[:id]) : Tag.find_by_name(params[:id])
   end
 
 end
