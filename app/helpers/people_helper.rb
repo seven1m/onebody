0450e76b46520f5f2f5fffb4d2f203dc6322f43b
Tim Morgan <tim@timmorgan.org>
2010-11-06 21:16:59 -0500

    A usable profile page.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -4,8 +4,8 @@ module PeopleHelper
   def linkify(text, attribute)
     text = h(text)
     text.split(/,\s*/).map do |item|
-      link_to item, search_path(attribute => item), :class => 'no-underline'
-    end.join ', '
+      link_to item, search_path(attribute => item)
+    end
   end
 
   def attribute(attribute, &block)
