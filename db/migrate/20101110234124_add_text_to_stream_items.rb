4a59e75d6df2d88c852365ec7ce3940fb13e2aa2
Tim Morgan <tim@timmorgan.org>
2010-11-20 16:16:56 -0600

    Fix migration.

--- db/migrate/20101110234124_add_text_to_stream_items.rb
+++ db/migrate/20101110234124_add_text_to_stream_items.rb
@@ -1,9 +1,9 @@
 class AddTextToStreamItems < ActiveRecord::Migration
-  change_table :stream_items do |t|
-    t.boolean :text, :default => false
-  end
-  StreamItem.reset_column_information
   def self.up
+    change_table :stream_items do |t|
+      t.boolean :text, :default => false
+    end
+    StreamItem.reset_column_information
     Site.each do
       Message.all.each do |message|
         message.update_stream_items
@@ -12,8 +12,8 @@ class AddTextToStreamItems < ActiveRecord::Migration
   end
 
   def self.down
-  change_table :stream_items do |t|
-    t.remove :text
-  end
+    change_table :stream_items do |t|
+      t.remove :text
+    end
   end
 end
