fdfdcc9bb501dcadb77d8d497868af65ab9ef731
Tim Morgan <tim@timmorgan.org>
2010-12-04 19:45:47 -0600

    Fixes for IE styling issues.

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,2 +1,4 @@
-$('.favorites-inline :last-child, #common-nav :last-child').addClass('last-child');
-$('h2:first-child, h3:first-child').addClass('first-child');
+$(function(){
+  $('.favorites-inline :last-child, #common-nav :last-child').addClass('last-child');
+  $('h2:first-child, h3:first-child').addClass('first-child');
+});
