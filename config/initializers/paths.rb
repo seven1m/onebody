5a68cd75a972bc89232e0efbfa1c264d1adb6eb8
Tim Morgan <tim@timmorgan.org>
2010-12-05 14:11:08 -0600

    Update paths and overrides for old file locations.
    DEPRECATION: this will be removed in a future version of OneBody, since
    we have now moved to Paperclip, and all files are stored in
    public/system, which is symlinked by Capistrano to the shared directory.

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -2,5 +2,4 @@ unless defined? DB_PHOTO_PATH
   DB_PHOTO_PATH        = Rails.root.join('db/photos')
   DB_PUBLICATIONS_PATH = Rails.root.join('db/publications')
   DB_ATTACHMENTS_PATH  = Rails.root.join('db/attachments')
-  DB_TASK_FILES_PATH   = Rails.root.join('db/task_files')
 end
--- lib/recipes/templates/links.rb
+++ lib/recipes/templates/links.rb
@@ -1,5 +1,5 @@
-DB_PHOTO_PATH        = "<%= shared_path + '/db/photos' %>"
-DB_PUBLICATIONS_PATH = "<%= shared_path + '/db/publications' %>"
-DB_ATTACHMENTS_PATH  = "<%= shared_path + '/db/attachments' %>"
-DB_TASK_FILES_PATH   = "<%= shared_path + '/db/task_files' %>"
-DEPLOY_THEME_DIR     = "<%= shared_path + '/themes' %>"
+SHARED_PATH          = Pathname.new("<%= shared_path %>")
+DB_PHOTO_PATH        = SHARED_PATH.join('db/photos')
+DB_PUBLICATIONS_PATH = SHARED_PATH.join('db/publications')
+DB_ATTACHMENTS_PATH  = SHARED_PATH.join('db/attachments')
+DEPLOY_THEME_DIR     = SHARED_PATH.join('db/themes')
