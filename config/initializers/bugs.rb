38512b50ff254c73fb09b4ce9457906e38723823
Tim Morgan <tim@timmorgan.org>
2010-12-05 14:10:25 -0600

    Fix bug notification email.

--- config/initializers/bugs.rb
+++ config/initializers/bugs.rb
@@ -1,6 +1,6 @@
 begin
   if (recipient = Setting.get(:contact, :bug_notification_email)).to_s.any?
-    OneBody.config.middleware.use ExceptionNotifier,
+    OneBody::Application.config.middleware.use ExceptionNotifier,
       :email_prefix         => "[OneBody] ",
       :sender_address       => recipient,
       :exception_recipients => [recipient]
