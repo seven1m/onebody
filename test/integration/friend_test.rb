c02bcdabdd84e4701dbcb36c069545b73e3e52be
Tim Morgan <tim@timmorgan.org>
2010-12-18 02:09:58 -0600

    Fix friend integration test.

--- test/integration/friend_test.rb
+++ test/integration/friend_test.rb
@@ -11,34 +11,29 @@ class FriendTest < ActionController::IntegrationTest
     sign_in_as people(:jeremy)
 
     view_profile people(:tim)
-    assert_select '#subnav', :html => /Remove from Friends/
-    get '/directory/browse'
+    assert_select 'body', :html => /Remove from Friends/
     assert_select '#add_friend_' + people(:tim).id.to_s, :count => 0
 
-    view_profile people(:jane)
-    assert_select '#subnav', :html => /Add to Friends/
-    get '/search', :browse => true
-    assert_select '#add_friend_' + people(:jane).id.to_s
+    view_profile people(:jennie)
+    assert_select 'body', :html => /Add to Friends/
+    assert_select '#add_friend_' + people(:jennie).id.to_s
 
-    post "/people/#{people(:jeremy).id}/friends?friend_id=#{people(:jane).id}"
+    post "/people/#{people(:jeremy).id}/friends?friend_id=#{people(:jennie).id}"
     assert_response :success
     assert_select 'body', :html => /friend request has been sent/
 
-    get '/search', :browse => true
-    assert_select '#add_friend_' + people(:jane).id.to_s, :count => 0
-    view_profile people(:jane)
-    assert_select '#subnav', :html => /friend request pending/
+    view_profile people(:jennie)
+    assert_select '#add_friend_' + people(:jennie).id.to_s, :count => 0
+    assert_select 'body', :html => /friend request pending/
 
-    sign_in_as people(:jane)
+    sign_in_as people(:jennie), 'password'
     assert_select 'body', :html => /pending friend requests/
-    f = people(:jane).friendship_requests.find_by_from_id(people(:jeremy).id)
+    f = people(:jennie).friendship_requests.find_by_from_id(people(:jeremy).id)
 
-    put "/people/#{people(:jane).id}/friends/#{f.id}?accept=true"
+    put "/people/#{people(:jennie).id}/friends/#{f.id}?accept=true"
     assert_response :redirect
     view_profile people(:jeremy)
-    assert_select '#subnav', :html => /Remove from Friends/
-
-    get '/search', :browse => true
+    assert_select 'body', :html => /Remove from Friends/
     assert_select '#add_friend_' + people(:jeremy).id.to_s, :count => 0
   end
 end
