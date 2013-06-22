fbb609115a8a31e32fe878fb6be02e9eeb59fe12
Tim Morgan <tim@timmorgan.org>
2013-06-21 21:04:08 -0500

    Set default encoding to utf8.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+Mysql2::Client::CHARSET_MAP['latin1'] = Encoding::UTF_8
+
 # Initialize the rails application
 OneBody::Application.initialize!
