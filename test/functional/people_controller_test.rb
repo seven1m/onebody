d1dd6480a2acd94547a079d2c77f58984c231f84
Tim Morgan <tim@timmorgan.org>
2013-06-24 00:16:37 -0500

    Improve the way we require the test_helper in tests.

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require_relative '../test_helper'
 
 GLOBAL_SUPER_ADMIN_EMAIL = 'support@example.com' unless defined?(GLOBAL_SUPER_ADMIN_EMAIL) and GLOBAL_SUPER_ADMIN_EMAIL == 'support@example.com'
 
