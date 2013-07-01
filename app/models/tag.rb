8bdd21b95825305c8adb5c33d255314de4e0ea06
Tim Morgan <tim@timmorgan.org>
2013-06-30 22:18:06 -0500

    Use Rubygems find_by_name instead.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
-path = ActsAsTaggable::Engine.config.eager_load_paths.grep(/models/).first
-require File.join(path, 'tag')
+gem_dir = Gem::Specification.find_by_name("acts_as_taggable_on_steroids").gem_dir
+require File.join(gem_dir, 'app/models/tag')
 
 Tag.class_eval { include TagConcern }
