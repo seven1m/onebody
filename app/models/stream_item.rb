86d6223236a2bb9509aa0c4eb798f6d81b957018
Tim Morgan <tim@timmorgan.org>
2013-07-02 20:09:31 -0500

    No need to manually set serialized class.

--- app/models/stream_item.rb
+++ app/models/stream_item.rb
@@ -4,15 +4,10 @@ class StreamItem < ActiveRecord::Base
   belongs_to :group
   belongs_to :streamable, polymorphic: true
 
-  serialize :context
+  serialize :context, Hash
 
   scope_by_site_id
 
-  before_save :ensure_context_is_hash
-
-  def ensure_context_is_hash
-    self.context = {} if not context.is_a?(Hash)
-  end
   def can_have_comments?
     %w(Verse Note Album).include?(streamable_type)
   end
