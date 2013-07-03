d5a72e0c494a68a50d31130c5d72eb560a707adf
Tim Morgan <tim@timmorgan.org>
2013-07-03 18:06:57 -0500

    Refactor Ability class.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,45 @@ class Ability
   include CanCan::Ability
 
   def initialize(person)
+    @person = person
+    allow_people
+    allow_families
+    allow_groups
+    allow_albums
+    allow_messages
+  end
 
-    # person
-    can :update, person
-    can :update, Person, family: person.family, deleted: false if person.adult?
-    can :manage, Person if person.admin?(:edit_profiles)
+  private
 
-    # family
-    can :update, person.family if person.adult?
-    can :manage, Family if person.admin?(:edit_profiles)
+  def allow_people
+    can :update, @person
+    can :update, Person, family: @person.family, deleted: false if @person.adult?
+    can :manage, Person if @person.admin?(:edit_profiles)
+  end
 
-    # group
-    can [:update, :destroy], Group, memberships: {person: person, admin: true}
-    can :manage, Group if person.admin?(:manage_groups)
+  def allow_families
+    can :update, @person.family if @person.adult?
+    can :manage, Family if @person.admin?(:edit_profiles)
+  end
 
-    # album
-    can [:update, :destroy], Album, person: person
-    can :manage, Album if person.admin?(:manage_pictures)
+  def allow_groups
+    can [:update, :destroy], Group, memberships: {person: @person, admin: true}
+    can :manage, Group if @person.admin?(:manage_groups)
+  end
 
-    # message
-    can [:update, :destroy], Message, person: person
-    can [:update, :destroy], Message, group: {memberships: {person: person, admin: true}}
-    if person.admin?(:manage_groups)
+  def allow_albums
+    can [:update, :destroy], Album, person: @person
+    can :manage, Album if @person.admin?(:manage_pictures)
+  end
+
+  def allow_messages
+    can [:update, :destroy], Message, person: @person
+    can [:update, :destroy], Message, group: {memberships: {person: @person, admin: true}}
+    if @person.admin?(:manage_groups)
       can :manage, Message do |message|
         message.group_id # belongs to a group
       end
     end
   end
+
 end
