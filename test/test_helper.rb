5fe43dba00ac9b0c7e82a2ff4ac2ce6571213ae2
Tim Morgan <tim@timmorgan.org>
2014-03-29 23:05:25 -0500

    Fix tests.
    Bizarre it was working before. Not sure why this was needed now...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/application', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 #require 'notifier'
 require 'rake'
