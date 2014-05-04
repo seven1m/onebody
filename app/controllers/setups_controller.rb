447e219c22870120e8bfc04237dd1811d1496bcc
Tim Morgan <tim@timmorgan.org>
2014-05-03 21:39:07 -0500

    Refactor setup into model and slim controller.

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -12,49 +12,13 @@ class SetupsController < ApplicationController
   end
 
   def create
-    params.permit! # FIXME
-    Person.transaction do
-      @person = Person.new
-      if params[:domain_name].to_s.any?
-        Site.current.host = params[:domain_name]
-        Site.current.save
-      else
-        @person.errors.add :base, t('setup.invalid_domain_name')
-        render action: 'new'
-        return
-      end
-      @person.attributes = params[:person]
-      @person.password = params[:password].present? ? params[:password] : nil
-      @person.password_confirmation = params[:password_confirmation].present? ? params[:password_confirmation] : nil
-      unless @person.password and @person.password == @person.password_confirmation
-        @person.errors.add :error, t('accounts.set_password_error')
-        render action: 'new'
-        return
-      end
-      unless @person.email.to_s.any?
-        @person.errors.add :email, t('activerecord.errors.models.person.attributes.email.invalid')
-        render action: 'new'
-        return
-      end
-      @person.can_sign_in = true
-      @person.visible_to_everyone = true
-      @person.visible_on_printed_directory = true
-      @person.full_access = true
-      @person.child = false
-      @person.family = Family.create!(
-        name:      @person.name,
-        last_name: @person.last_name
-      )
-      @person.admin = Admin.create!(super_admin: true)
-      if @person.save
-        Setting.set_global('Contact', 'Bug Notification Email', @person.email)
-        Setting.set_global('Contact', 'Tech Support Email', @person.email)
-        flash[:notice] = t('setup.complete')
-        redirect_to new_session_path(from: '/stream')
-      else
-        render action: 'new'
-        raise ActiveRecord::Rollback
-      end
+    @setup = Setup.new(params.permit!)
+    if @setup.execute!
+      flash[:notice] = t('setup.complete')
+      redirect_to new_session_path(from: '/stream')
+    else
+      @person = @setup.person
+      render action: 'new'
     end
   end
 
