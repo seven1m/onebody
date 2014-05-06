04099c33b1beff8676eb6b6cbfe21f19bcdf8d7e
Tim Morgan <tim@timmorgan.org>
2014-05-06 08:29:42 -0500

    Simplify test.

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -2,13 +2,11 @@ require_relative '../spec_helper'
 
 describe NewsItem do
 
-  context 'Published Date' do
+  describe 'published' do
 
     it 'should not overwrite published date if it has been specified' do
-      last_month = Date.today - 30
-      @news_item = FactoryGirl.create(:news_item, published: last_month)
-      expect(@news_item.published.strftime("%m/%d/%Y")).to eq(last_month.strftime("%m/%d/%Y"))
-      expect(@news_item.created_at > last_month).to be
+      @news_item = FactoryGirl.create(:news_item, published: Date.current - 30)
+      expect(@news_item.published).to be < Date.current
     end
 
     it 'should update published date if it is unspecified' do
