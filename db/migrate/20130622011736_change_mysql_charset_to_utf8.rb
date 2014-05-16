9e2ca3aafa5d30ed2215374ffa428f4b16b03083
Tim Morgan <tim@timmorgan.org>
2014-05-16 18:04:56 -0500

    Simplify migration for utf8.

--- db/migrate/20130622011736_change_mysql_charset_to_utf8.rb
+++ db/migrate/20130622011736_change_mysql_charset_to_utf8.rb
@@ -1,15 +1,10 @@
 class ChangeMysqlCharsetToUtf8 < ActiveRecord::Migration
   def self.up
     db = Rails.configuration.database_configuration[Rails.env]['database']
-    columns = ActiveRecord::Base.connection.select_rows("SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, CHARACTER_MAXIMUM_LENGTH  FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='#{db}';")
-    columns.each do |table, column, type, limit|
-      next unless type =~ /varchar|text/
-      new_type = type.sub(/varchar/i, 'varbinary').sub(/text/i, 'blob')
-      puts "converting #{table} #{column}..."
-      # thanks to Mike Perham for this tip
-      # http://www.mikeperham.com/2012/03/31/converting-a-mysql-database-from-latin1-to-utf8/
-      ActiveRecord::Base.connection.execute("ALTER TABLE #{table} CHARACTER SET utf8 COLLATE utf8_unicode_ci, CHANGE #{column} #{column} #{new_type};")
-      ActiveRecord::Base.connection.execute("ALTER TABLE #{table} CHANGE #{column} #{column} #{type} CHARACTER SET utf8 COLLATE utf8_unicode_ci;")
+    tables = ActiveRecord::Base.connection.select_rows("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='#{db}';")
+    tables.each do |row|
+      puts "converting #{row[0]} to utf8..."
+      ActiveRecord::Base.connection.execute("ALTER TABLE #{row[0]} CONVERT TO CHARACTER SET utf8;")
     end
     puts "done!"
   end
