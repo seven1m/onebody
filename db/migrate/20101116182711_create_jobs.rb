fdde5f9e121a83a205efe5da99653f0948dbf317
Tim Morgan <tim@timmorgan.org>
2014-05-15 21:29:35 -0500

    Fix migration.

--- db/migrate/20101116182711_create_jobs.rb
+++ db/migrate/20101116182711_create_jobs.rb
@@ -2,7 +2,6 @@ class CreateJobs < ActiveRecord::Migration
   def self.up
     create_table :jobs do |t|
       t.integer :site_id
-      t.integer :id
       t.string :command, :limit => 255
       t.timestamps
     end
