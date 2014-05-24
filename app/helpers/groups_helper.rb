db1f3551866afb9828d5903c26e5142cca12b5fa
Tim Morgan <tim@timmorgan.org>
2014-05-24 17:57:00 -0500

    Begin work on group pages.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -10,4 +10,19 @@ module GroupsHelper
       'box-success'
     end
   end
+
+  def group_avatar_path(group, size=:tn)
+    if group.try(:photo).try(:exists?)
+      group.photo.url(size)
+    else
+      size = :large unless size == :tn # we only have only two sizes
+      image_path("group.#{size}.jpg")
+    end
+  end
+
+  def group_avatar_tag(group, options={})
+    options.reverse_merge!(size: :tn, alt: group.try(:name))
+    options.reverse_merge!(class: "avatar #{options[:size]}")
+    image_tag(group_avatar_path(group, options.delete(:size)), options)
+  end
 end
