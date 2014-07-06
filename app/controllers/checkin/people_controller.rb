8809b2625c51ee25a677ccec2576170f2a1cb8fd
Tim Morgan <tim@timmorgan.org>
2014-07-05 22:33:28 -0500

    Select all columns needed for child concern.

--- app/controllers/checkin/people_controller.rb
+++ app/controllers/checkin/people_controller.rb
@@ -1,9 +1,9 @@
 class Checkin::PeopleController < ApplicationController
 
   def index
-    select = 'families.id, families.barcode_id, people.family_id, people.id, people.first_name, people.last_name, people.suffix, people.classes, people.medical_notes, people.can_pick_up, people.cannot_pick_up'
+    select = 'families.id, families.barcode_id, people.family_id, people.id, people.first_name, people.last_name, people.suffix, people.classes, people.medical_notes, people.can_pick_up, people.cannot_pick_up, people.child, people.birthday'
     if params[:family_barcode_id]
-      @people = Person.all(:joins => :family, :conditions => ["(families.barcode_id = ? or families.alternate_barcode_id = ?) and people.deleted = ?", params[:family_barcode_id], params[:family_barcode_id], false], :select => select)
+      @people = Person.undeleted.joins(:family).where("(families.barcode_id = ? or families.alternate_barcode_id = ?)", params[:family_barcode_id], params[:family_barcode_id]).select(select)
     elsif params[:q]
       search = Search.new_from_params(:family_name => params[:q])
       if (families = search.query(nil, 'family')).any?
