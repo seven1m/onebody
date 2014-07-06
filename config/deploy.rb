e3ede806b3e851bd0d732b92f3358c468ad339f6
Tim Morgan <tim@timmorgan.org>
2014-07-05 19:56:24 -0500

    Link some directories on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :deploy_to, '/var/www/apps/onebody'
 
 set :linked_files, %w{config/database.yml config/email.yml config/secrets.yml}
 
+set :linked_dirs, %w{log tmp public/system db/photos db/attachments}
+
 namespace :deploy do
 
   desc 'Restart application'
