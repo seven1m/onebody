26518a0d5fd13458907ffc10350fdc2afbd57a84
Tim Morgan <tim@timmorgan.org>
2014-07-10 19:48:41 -0500

    Let the Mail gem get the body, and we'll get decoded UTF-8 [fixes #132]

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -222,9 +222,6 @@ class Notifier < ActionMailer::Base
       return
     end
 
-    body[:text].encode!('utf-8', invalid: :replace, undef: :replace) if body[:text]
-    body[:html].encode!('utf-8', invalid: :replace, undef: :replace) if body[:html]
-
     @message_sent_to_group = false
     sent_to_count = 0
 
@@ -396,31 +393,17 @@ class Notifier < ActionMailer::Base
     end
 
     def self.get_body(email)
-      # if the message is multipart, try to grab the plain text and/or html parts
-      text = nil
-      html = nil
-      if email.multipart?
-        email.parts.each do |part|
-          case part.content_type.downcase.split(';').first
-            when 'text/plain'
-              text = part.body.to_s
-            when 'text/html'
-              html = part.body.to_s
-            when 'multipart/alternative'
-              if p = part.parts.detect { |p| p.content_type.downcase.split(';').first == 'text/plain' }
-                text ||= p.body.to_s
-              end
-              if p = part.parts.detect { |p| p.content_type.downcase.split(';').first == 'text/html'  }
-                html ||= p.body.to_s
-              end
-          end
-        end
-        return {text: text, html: html}
-      elsif email.content_type.downcase.split(';').first == 'text/html'
-        return {text: nil, html: email.body.to_s}
-      else
-        return {text: email.body.to_s}
+      body = {
+        text: email.text_part.try(:decoded),
+        html: email.html_part.try(:decoded)
+      }
+      type = email.content_type.downcase.split(';').first
+      if body[:text].nil? and type == 'text/plain'
+        body[:text] = email.decoded
+      elsif body[:html].nil? and type == 'text/html'
+        body[:html] = email.decoded
       end
+      body
     end
 
     def clean_body(body)
