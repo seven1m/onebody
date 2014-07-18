f2a48f0da18f119e05d83a918a90e59de8c432c6
Tim Morgan <tim@timmorgan.org>
2014-07-17 23:29:40 -0500

    Swallow connection errors.

--- config/initializers/_settings.rb
+++ config/initializers/_settings.rb
@@ -1,3 +1,7 @@
 SETTINGS = {}
 
-Setting.update_all if Setting.table_exists?
+begin
+  Setting.update_all if Setting.table_exists?
+rescue Mysql2::Error
+  # no connection probably
+end
--- config/initializers/pages.rb
+++ config/initializers/pages.rb
@@ -1,5 +1,9 @@
-if Site.table_exists? and Page.table_exists?
-  Site.each do |site|
-    site.add_pages
+begin
+  if Site.table_exists? and Page.table_exists?
+    Site.each do |site|
+      site.add_pages
+    end
   end
+rescue Mysql2::Error
+  # no connection probably
 end
