63e261faa78e04323023f79dba9782421c5f338b
Eric Farkas <eric@prudentiadigital.com>
2014-07-27 22:30:42 -0400

    Spec and fix for Administration::DashboardsController

---
+++ spec/controllers/administration/dashboards_controller_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../spec_helper'
+
+describe Administration::DashboardsController, "GET /show" do
+  render_views
+
+  context "an unauthorized user" do
+    let!(:person) { FactoryGirl.create(:person) }
+    
+    it 'should return unauthorized' do
+      get :show, nil, { logged_in_id: person.id }
+      expect(response.status).to eq(401)
+    end
+  end
+  
+  context "an authorized user" do
+    let!(:admin) { FactoryGirl.create(:person, :admin_manage_updates) }
+    
+    it 'should return unauthorized' do
+      get :show, nil, { logged_in_id: admin.id }
+      expect(response.status).to eq(200)
+    end
+  end
+end
