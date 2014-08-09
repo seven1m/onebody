b4d26a765debe214d4159faee10bb1157e8a8ead
Han-Sheng Chin <hschin@gmail.com>
2014-08-09 16:59:27 +0800

    Work on displaying list of donations

--- app/controllers/administration/donations_controller.rb
+++ app/controllers/administration/donations_controller.rb
@@ -3,10 +3,21 @@ class Administration::DonationsController < ApplicationController
   before_filter :only_admins
 
   def index
+    @donations = Donation.all
   end
 
   def new
     @donation = Donation.new
   end
 
+  def create
+    @donation = Donation.new(donation_params)
+    @donation.save
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:date, :person_id, :family_id, :donation_type, :name, :amount_cents, :amount_currency)
+  end
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,6 @@
 class Donation < ActiveRecord::Base
   belongs_to :person
   belongs_to :family
+
+  monetize :amount_cents
 end
