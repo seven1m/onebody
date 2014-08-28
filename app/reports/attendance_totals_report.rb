9bd39393f3ced35390f2bcfa5281a248bef918c5
Tim Morgan <tim@timmorgan.org>
2014-08-27 22:19:45 -0500

    Fix date parsing and presentation.

--- app/reports/attendance_totals_report.rb
+++ app/reports/attendance_totals_report.rb
@@ -1,20 +1,25 @@
 class AttendanceTotalsReport < Dossier::Report
 
   def sql
- 	  Group.select("name").joins(:attendance_records).select(:attended_at, "count(*) as
-  att_count").references(:attendance_records).where("attended_at >= :fromdate and attended_at <= :thrudate").group("name", "attended_at").to_sql
+    Group.select(:name)
+         .joins(:attendance_records)
+         .select(:attended_at, 'count(*) as att_count')
+         .references(:attendance_records)
+         .where('attended_at >= :fromdate and attended_at <= :thrudate')
+         .group(:name, :attended_at)
+         .to_sql
   end
 
   def fromdate
-    Date.strptime(options[:fromdate], Setting.get(:formats, :date)) unless options[:fromdate].nil?
+    Date.parse_in_locale(options[:fromdate].to_s) || (Date.current - 1.week)
   end
 
   def thrudate
-    Date.strptime(options[:thrudate], Setting.get(:formats, :date)) unless options[:thrudate].nil?
+    Date.parse_in_locale(options[:thrudate].to_s) || Date.current
   end
 
   def format_attended_at(value)
-    value.strftime(Setting.get(:formats, :date))
+    value.to_s(:date) if value.is_a?(Time)
   end
 
 end
