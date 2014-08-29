e8b2364e44d71c3e25df8fd7d7c906f5eafc5dcc
Josue Lima <josuedsi@gmail.com>
2014-08-29 14:50:53 -0300

    Better place for cleaning up stream table #223

--- db/migrate/20140615223016_add_stream_item_for_sites.rb
+++ db/migrate/20140615223016_add_stream_item_for_sites.rb
@@ -1,5 +1,5 @@
 class AddStreamItemForSites < ActiveRecord::Migration
   def change
-    Site.each(&:create_as_stream_item)
+    Site.each(&:create_as_stream_item) unless Rails.env.test?
   end
 end
