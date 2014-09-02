0b239c58d389a03e124a84264cf9586c1fa294ac
hiren mistry <hiren.mistry@chai-monsters.com>
2014-09-02 00:51:28 -0700

    Removed notes from more migrations

--- db/migrate/20090721210806_remove_deleted_from_notes.rb
+++ db/migrate/20090721210806_remove_deleted_from_notes.rb
@@ -1,6 +1,5 @@
 class RemoveDeletedFromNotes < ActiveRecord::Migration
   def self.up
-    Site.each { Note.where(deleted: true).each { |n| n.destroy } }
     change_table :notes do |t|
       t.remove :deleted
     end
