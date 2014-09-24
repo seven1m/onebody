8286f2c4384be46d001d936244e12114518a4cfa
Justin Richter <jrichter@jetfive.com>
2014-09-23 19:55:29 -0500

    Fix [#286] - add basic_auth to request and catch failures

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,22 +3,36 @@ class Email
   include HTTParty
 
   def self.show_routes(skip: 0, limit: 1)
-    self.get("/routes", params: {skip: skip, limit: limit})
+    key = Setting.get(:email, :mailgunapikey)
+    get('https://api.mailgun.net/v2/routes',
+        basic_auth: { username: 'api', password: key },
+        params: { skip: skip, limit: limit })
   end
 
   def self.create_catch_all
-    routes = self.show_routes(limit: 100)
-    match = []
-    routes["items"].each do |item|
-      if item["description"] == "Catch All Route - Created By OneBody" and item["expression"] == "match_recipient('.*@#{Site.current.email_host}')"
-        match << item
-      end
-    end
-    if match.empty?
-      key = Setting.get(:email, :mailgunapikey)
-      self.post("https://api:#{key}@api.mailgun.net/v2/routes", body: self.build_data)
+    routes = show_routes(limit: 100)
+  rescue Exception => e
+    { 'message' => 'error' }
+    Rails.logger.error(e.message)
+  else
+    if routes.to_s == 'Forbidden'
+      { 'message' => 'apikey' }
     else
-      {"message"=>"Route found."}
+      match = []
+      routes['items'].each do |item|
+        if item['description'] == 'Catch All Route - Created By OneBody' and
+          item['expression'] == "match_recipient('.*@#{Site.current.email_host}')"
+          match << item
+        end
+      end
+      if match.empty?
+        key = Setting.get(:email, :mailgunapikey)
+        post('https://api.mailgun.net/v2/routes',
+             basic_auth: { username: 'api', password: key },
+             body: build_data)
+      else
+        { 'message' => 'Route found.' }
+      end
     end
   end
 
@@ -27,7 +41,7 @@ class Email
   def self.build_data
     data = {}
     data[:priority] = 0
-    data[:description] = "Catch All Route - Created By OneBody"
+    data[:description] = 'Catch All Route - Created By OneBody'
     data[:expression] = "match_recipient('.*@#{Site.current.email_host}')"
     data[:action] = ["forward('http://#{Site.current.host}/emails.mime')", "stop()"]
     return data
