ffa807403cb11579abb37c5b627acc8971b7b88c
Tim Morgan <tim@timmorgan.org>
2015-01-05 21:19:52 -0600

    Remove unused Person#access_attributes method

--- app/models/person.rb
+++ app/models/person.rb
@@ -209,10 +209,6 @@ class Person < ActiveRecord::Base
     end
   end
 
-  def access_attributes
-    self.attributes.keys.grep(/_access$/).reject { |a| a == 'full_access' }
-  end
-
   # generates security code for grabbing feed(s) without logging in
   before_create :update_feed_code
   def update_feed_code
