f0aafd97f797fe46ccc3033f7689761b2e8a9141
Tim Morgan <tim@timmorgan.org>
2015-01-07 22:16:36 -0600

    Refactor privacy controller

---
+++ app/models/membership_sharing_updater.rb
@@ -0,0 +1,33 @@
+class MembershipSharingUpdater
+
+  def initialize(user, updates)
+    @user = user
+    @updates = updates
+  end
+
+  def perform
+    Array(@updates).each do |membership_id, sharing|
+      membership = Membership.find(membership_id)
+      verify_authorization(membership)
+      sharing.each do |attribute, value|
+        next unless attribute =~ /\Ashare_/
+        value = false if shared_at_person_level?(membership, attribute)
+        membership.attributes = { attribute => value }
+      end
+      membership.save!
+    end
+  end
+
+  private
+
+  def verify_authorization(membership)
+    unless membership.updatable_by?(@user)
+      raise Authority::SecurityViolation.new
+    end
+  end
+
+  def shared_at_person_level?(membership, attribute)
+    membership.person.attributes[attribute]
+  end
+
+end
--- spec/controllers/privacies_controller_spec.rb
+++ spec/controllers/privacies_controller_spec.rb
@@ -2,107 +2,142 @@ require_relative '../rails_helper'
 
 describe PrivaciesController, type: :controller do
 
-  before do
-    @group = FactoryGirl.create(:group)
-    @user = FactoryGirl.create(:person) # peter
-    @membership = @group.memberships.create(person: @user)
+  let(:group)      { FactoryGirl.create(:group) }
+  let(:user)       { FactoryGirl.create(:person) }
+  let(:membership) { group.memberships.create(person: user) }
+
+  describe '#edit' do
+    context 'editing my own profile' do
+      before do
+        get :edit, { person_id: user.id }, { logged_in_id: user.id }
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    context "editing someone else's profile" do
+      let(:stranger) { FactoryGirl.create(:person) }
+
+      before do
+        get :edit, { person_id: stranger.id }, { logged_in_id: user.id }
+      end
+
+      it 'returns unauthorized' do
+        expect(response).to be_unauthorized
+      end
+
+      context 'user is an adult in the same family' do
+        let(:user) { FactoryGirl.create(:person, family: stranger.family) }
+
+        it 'renders the edit template' do
+          expect(response).to render_template(:edit)
+        end
+      end
+
+      context 'user is an admin' do
+        let(:user) { FactoryGirl.create(:person, :admin_edit_profiles) }
+
+        it 'renders the edit template' do
+          expect(response).to render_template(:edit)
+        end
+      end
+    end
   end
 
-  it "should redirect to edit action" do
-    get :show, {group_id: @group.id, membership_id: @membership.id},
-      {logged_in_id: @user.id}
-    expect(response).to redirect_to(edit_group_membership_privacy_path(@group, @membership))
-    get :show, {person_id: @user.id}, {logged_in_id: @user.id}
-    expect(response).to redirect_to(edit_person_privacy_path(@user, anchor: "p#{@user.id}"))
-  end
+  describe '#update' do
+    context "editing someone else's profile" do
+      let(:stranger) { FactoryGirl.create(:person) }
 
-  it "should edit membership privacy" do
-    @user.update_attributes!(
-      share_address:      false,
-      share_mobile_phone: false,
-      share_home_phone:   false,
-      share_work_phone:   false,
-      share_fax:          false,
-      share_email:        false,
-      share_birthday:     false,
-      share_anniversary:  false
-    )
-    get :edit, {person_id: @user.id}, {logged_in_id: @user.id}
-    expect(response).to be_success
-    expect(response).to render_template(:edit)
-    post :update, {
-      person_id: @user.id,
-      family: {
-        people_attributes: {}
-      },
-      memberships: {
-        @membership.id => {
-          share_address:      true,
-          share_mobile_phone: true,
-          share_home_phone:   true,
-          share_work_phone:   false,
-          share_fax:          false,
-          share_email:        true,
-          share_birthday:     false,
-          share_anniversary:  false
-        }
-      }
-    }, {logged_in_id: @user.id}
-    expect(response).to redirect_to(person_path(@user.id))
-    expect( @membership.reload).to be_share_address
-    expect( @membership).to be_share_mobile_phone
-    expect( @membership).to be_share_home_phone
-    expect(@membership).to_not be_share_work_phone
-    expect(@membership).to_not be_share_fax
-    expect( @membership).to be_share_email
-    expect(@membership).to_not be_share_birthday
-    expect(@membership).to_not be_share_anniversary
-  end
+      before do
+        patch :update, {
+          person_id: stranger.id,
+          family: { visible: '0' }
+        }, { logged_in_id: user.id }
+      end
 
-  it "should only allow admins and adult family members to edit privacy" do
-    @stranger = FactoryGirl.create(:person)
-    @admin = FactoryGirl.create(:person, :super_admin)
-    get :edit, {person_id: @user.id}, {logged_in_id: @stranger.id}
-    expect(response).to be_unauthorized
-    post :update, {person_id: @user.id, person: {}}, {logged_in_id: @stranger.id}
-    expect(response).to be_unauthorized
-    get :edit, {person_id: @user.id}, {logged_in_id: @admin.id}
-    expect(response).to be_success
-  end
+      it 'returns unauthorized' do
+        expect(response).to be_unauthorized
+      end
+    end
 
-  it "should edit family privacy" do
-    get :edit, {person_id: @user.id}, {logged_in_id: @user.id}
-    expect(response).to be_success
-    expect(response).to render_template('edit')
-    post :update, {
-      person_id: @user.id,
-      family: {
-        people_attributes: {
-          '0' => {
-            id:                 @user.id,
-            share_address:      true,
-            share_mobile_phone: true,
-            share_home_phone:   true,
-            share_work_phone:   false,
-            share_fax:          false,
-            share_email:        true,
-            share_birthday:     false,
-            share_anniversary:  false,
-            share_activity:     false
+    context 'updating my own profile' do
+      before do
+        patch :update, {
+          person_id: user.id,
+          family: {
+            people_attributes: {
+              '0' => {
+                id:                 user.id,
+                share_mobile_phone: '1',
+                share_home_phone:   '0'
+              }
+            }
           }
-        }
-      }
-    }, {logged_in_id: @user.id}
-    expect(response).to redirect_to(person_path(@user.id))
-    expect( @user.reload).to be_share_address
-    expect( @user).to be_share_mobile_phone
-    expect( @user).to be_share_home_phone
-    expect(@user).to_not be_share_work_phone
-    expect(@user).to_not be_share_fax
-    expect( @user).to be_share_email
-    expect(@user).to_not be_share_birthday
-    expect(@user).to_not be_share_anniversary
-    expect(@user).to_not be_share_activity
+        }, { logged_in_id: user.id }
+      end
+
+      it 'updates privacy on the person record and redirects' do
+        expect(user.reload.share_mobile_phone?).to eq(true)
+        expect(user.reload.share_home_phone?).to eq(false)
+        expect(response).to redirect_to(person_path(user))
+      end
+    end
+
+    context 'updating group membership privacy' do
+      before do
+        patch :update, {
+          person_id: user.id,
+          family: {
+            people_attributes: {
+              '0' => {
+                id:                 user.id,
+                share_mobile_phone: '0'
+              }
+            }
+          },
+          memberships: {
+            membership.id => {
+              share_mobile_phone: '1'
+            }
+          }
+        }, { logged_in_id: user.id }
+      end
+
+      it 'updates privacy on the group membership record' do
+        expect(membership.reload.share_mobile_phone?).to eq(true)
+      end
+    end
+
+    context 'updating family visibility' do
+      before do
+        patch :update, {
+          person_id: user.id,
+          family: { visible: '0' }
+        }, { logged_in_id: user.id }
+      end
+
+      it 'changes family visibility' do
+        expect(user.family.reload.visible?).to eq(false)
+      end
+    end
+
+    context 'giving parental consent' do
+      let(:child) { FactoryGirl.create(:person, family: user.family, child: true) }
+
+      before do
+        patch :update, {
+          person_id: child.id,
+          agree: I18n.t('privacies.i_agree') + '.',
+          agree_commit: true
+        }, { logged_in_id: user.id }
+      end
+
+      it 'saves the consent on the child' do
+        expect(child.reload.parental_consent).to match(/John Smith \(\d+\) \d{4}-\d{2}-\d{2} \d+:\d+:\d+/)
+      end
+    end
   end
 
 end
