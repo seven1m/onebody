3b4c5a799f6552354612ce4e70c4d6a009b014d4
Tim Morgan <tim@timmorgan.org>
2015-01-24 16:31:56 -0600

    Refactor application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,7 @@
 # encoding: utf-8
 
 module ApplicationHelper
-
-  # TODO do we need these includes any more?
-  include TagsHelper
-  include PicturesHelper
-  include PhotosHelper
-
-  # TODO needed?
-  include ERB::Util
+  include TagsHelper # provided by acts_as_taggable_on_steroids
 
   FLASH_CLASSES = {
     info:    'flash callout callout-info',
@@ -20,7 +13,7 @@ module ApplicationHelper
 
   def flash_messages
     flash.map do |key, value|
-      if css_class = FLASH_CLASSES[key]
+      if (css_class = FLASH_CLASSES[key])
         content_tag(:div, preserve_breaks(value), class: css_class)
       else
         ''
@@ -28,9 +21,9 @@ module ApplicationHelper
     end.join.html_safe
   end
 
-  def preserve_breaks(text, make_safe=true)
+  def preserve_breaks(text, make_safe = true)
     text = h(text.to_s) if make_safe
-    text = text.gsub(/\n/, '<br/>').html_safe
+    text.gsub(/\n/, '<br/>').html_safe
   end
 
   def remove_excess_breaks(text)
@@ -43,39 +36,38 @@ module ApplicationHelper
     preserve_breaks(text, false)
   end
 
-  def simple_url(url, options={ www: true })
+  def simple_url(url, options = { www: true })
     if options[:www]
-      regex = /^https?:\/\//
+      regex = %r{\Ahttps?://}
     else
-      regex = /^https?:\/\/(www\.)?/
+      regex = %r{\Ahttps?://(www\.)?}
     end
     url.sub(regex, '').sub(/\/$/, '')
   end
 
   def safe_url(url)
-    if url =~ /\Ahttps?\:\/\/.+/
-      "#{url}" # wrap in new string so Hakiri is happy
-    end
+    return unless url =~ %r{\Ahttps?\://.+}
+    "#{url}" # wrap in new string so Hakiri is happy
   end
 
   def me?
-    @logged_in and @person and @logged_in == @person
+    @logged_in && @person && @logged_in == @person
   end
 
-  def help_path(name=nil)
+  def help_path(name = nil)
     page_for_public_path("help/#{name}")
   end
 
   def render_page_content(path)
-    if page = Page.where(path: path, published: true).first
-      sanitize_html(page.body)
-    end
+    page = Page.where(path: path, published: true).first
+    return unless page
+    sanitize_html(page.body)
   end
 
-  def format_phone(phone, mobile=false)
+  def format_phone(phone, mobile = false)
     format = Setting.get(:formats, mobile ? :mobile_phone : :phone)
     return phone if format.blank?
-    groupings = format.scan(/d+/).map { |g| g.length }
+    groupings = format.scan(/d+/).map(&:length)
     groupings = [3, 3, 4] unless groupings.length == 3
     ActionController::Base.helpers.number_to_phone(
       phone,
@@ -85,7 +77,7 @@ module ApplicationHelper
     )
   end
 
-  def link_to_phone(phone, options={})
+  def link_to_phone(phone, options = {})
     label = options.delete(:label)
     label ||= format_phone(phone, options.delete(:mobile))
     link_to label, "tel:#{phone.digits_only}", options
@@ -107,14 +99,17 @@ module ApplicationHelper
     else
       obj = form
     end
-    if obj.errors.any?
-      content_tag(:div, class: 'callout callout-danger form-errors') do
-        content_tag(:h4, t('There_were_errors')) +
-        content_tag(:ul, class: 'list') do
-          uniq_errors(obj).map do |attribute, message|
-            content_tag(:li, message, data: { attribute: "#{obj.class.name.underscore}_#{attribute}" })
-          end.join.html_safe
-        end
+    error_messages_for_object(obj)
+  end
+
+  def error_messages_for_object(obj)
+    return if obj.errors.empty?
+    content_tag(:div, class: 'callout callout-danger form-errors') do
+      content_tag(:h4, t('There_were_errors')) +
+      content_tag(:ul, class: 'list') do
+        uniq_errors(obj).map do |attribute, message|
+          content_tag(:li, message, data: { attribute: "#{obj.class.name.underscore}_#{attribute}" })
+        end.join.html_safe
       end
     end
   end
@@ -122,7 +117,7 @@ module ApplicationHelper
   def uniq_errors(object)
     # note: uniq doesn't work here since it isn't really an array
     seen = []
-    object.errors.select do |attr, message|
+    object.errors.select do |_attr, message|
       unless seen.include?(message)
         seen << message
         message
@@ -130,43 +125,29 @@ module ApplicationHelper
     end
   end
 
-  # TODO
-  def domain_name_from_url(url)
-    url =~ /^https?:\/\/([^\/]+)/
-    $1
-  end
-
-  def render_plugin_hook(name)
-    if hooks = PLUGIN_HOOKS[name]
-      hooks.map do |hook|
-        render partial: hook
-      end.join("\n").html_safe
-    end
-  end
-
-  def sortable_column_heading(label, sort, keep_params=[])
-    new_sort = (sort.split(',') + params[:sort].to_s.split(',')).uniq.join(',')
+  def sortable_column_heading(label, sort, keep_params = [])
     options = {
       controller: params[:controller],
       action:     params[:action],
       id:         params[:id],
-      sort:       new_sort
+      sort:       sort_params(sort)
     }.merge(
-      params.reject { |k, v| !keep_params.include?(k) }
+      params.reject { |k| !keep_params.include?(k) }
     )
     url = url_for(options)
     link_to label, url
   end
 
-  def params_without_action
-    controller.params_without_action
+  def sort_params(sort)
+    old_params = params[:sort].to_s.split(',')
+    (sort.split(',') + old_params).uniq.join(',')
   end
 
   def date_format
     placeholder = Setting.get(:formats, :date)
-      .gsub(/%Y/, I18n.t('date_format.YYYY'))
-      .gsub(/%m/, I18n.t('date_format.MM'))
-      .gsub(/%d/, I18n.t('date_format.DD'))
+                  .gsub(/%Y/, I18n.t('date_format.YYYY'))
+                  .gsub(/%m/, I18n.t('date_format.MM'))
+                  .gsub(/%d/, I18n.t('date_format.DD'))
     placeholder unless placeholder.include?('%')
   end
 
@@ -174,7 +155,7 @@ module ApplicationHelper
     date_format.downcase
   end
 
-  # TODO replace all inline JS links with unobtrusive JS
+  # TODO: replace all inline JS links with unobtrusive JS
   def link_to_function(*args, &block)
     options = args.extract_options!
     js = args.pop
@@ -196,35 +177,13 @@ module ApplicationHelper
     end
   end
 
-  def pagination(scope, options={})
+  def pagination(scope, options = {})
     options.reverse_merge!(renderer: BootstrapPagination::Rails)
     will_paginate scope, options
   end
 
-  def truncate_words(text, options={})
+  def truncate_words(text, options = {})
     truncate(text, options.reverse_merge(separator: ' ', omission: '…'))
-  end
-
-  def map_header(object)
-    if object.mapable?
-      data = { latitude: object.latitude,
-               longitude: object.longitude,
-               address: preserve_breaks(object.pretty_address),
-               notice: t('maps.notice'),
-               protocol: Setting.get(:features, :ssl) ? 'https' : 'http' }
-      content_for(:header) do
-        raw(
-          content_tag(:div, '', id: 'map', data: data) +
-          content_tag(:section, class: 'content-header map-overlay') do
-            breadcrumbs +
-            content_tag(:h1) do
-              sub = (s = content_for(:sub_title)) ? content_tag(:small, s) : ''
-              (@title + sub).html_safe
-            end
-          end
-        )
-      end
-    end
   end
 
   def analytics_js
@@ -246,9 +205,5 @@ module ApplicationHelper
         end
       end
     end
-  end
-
-  class << self
-    include ApplicationHelper
   end
 end
---
+++ app/helpers/maps_helper.rb
@@ -0,0 +1,30 @@
+module MapsHelper
+  def map_header(object)
+    return unless object.mapable?
+    content_for(:header) do
+      content_tag(:div, '', id: 'map', data: map_data(object)) +
+        content_tag(:section, class: 'content-header map-overlay') do
+          breadcrumbs +
+            content_tag(:h1) do
+              (@title + map_sub_title).html_safe
+            end
+        end
+    end
+  end
+
+  def map_sub_title
+    sub_title = content_for(:sub_title)
+    return '' unless sub_title
+    content_tag(:small, sub_title)
+  end
+
+  def map_data(object)
+    {
+      latitude: object.latitude,
+      longitude: object.longitude,
+      address: preserve_breaks(object.pretty_address),
+      notice: t('maps.notice'),
+      protocol: Setting.get(:features, :ssl) ? 'https' : 'http'
+    }
+  end
+end
