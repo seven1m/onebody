ad98a3013e0edd4ba7483fa7106820036626f6db
Anthony Underwood <email2ants@gmail.com>
2015-02-06 22:18:45 +0000

    Add S3 config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ OneBody::Application.configure do
     config.middleware.delete "Rack::Sendfile"
     config.middleware.use(Rack::Static, urls: ['/assets', '/images', '/system'], root: 'public')
   end
+
+  # paperclip s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
