07ea674525a89f90167d77d294e80d4c5fb99909
Tim Morgan <tim@timmorgan.org>
2015-04-20 22:53:36 -0500

    Update group memberships upon sync completion

---
+++ app/jobs/group_memberships_update_job.rb
@@ -0,0 +1,12 @@
+class GroupMembershipsUpdateJob < ActiveJob::Base
+  queue_as :group_memberships_update
+
+  def perform(site)
+    ActiveRecord::Base.connection_pool.with_connection do
+      Site.with_current(site) do
+        Group.update_memberships
+      end
+    end
+  end
+end
+
