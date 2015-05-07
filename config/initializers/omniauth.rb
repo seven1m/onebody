fc089ec5f48b65673e2b71c67824432f128fc862
Kevin Qiu <kevin@freshbooks.com>
2015-05-06 23:16:44 -0400

    Make sure Site.current is set before calling Setting.get

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  facebook_app_id = Setting.get(:facebook, :app_id)
-  facebook_app_secret = Setting.get(:facebook, :app_secret)
-  provider :facebook, facebook_app_id, facebook_app_secret, :scope => 'email,read_stream' if facebook_app_id and facebook_app_secret
+  if Site.current
+    facebook_app_id = Setting.get(:facebook, :app_id)
+    facebook_app_secret = Setting.get(:facebook, :app_secret)
+    provider :facebook, facebook_app_id, facebook_app_secret, :scope => 'email,read_stream' if facebook_app_id and facebook_app_secret
+  end
 end
