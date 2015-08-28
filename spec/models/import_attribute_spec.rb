63006b1495df4a01dec46cdc7a0f290a02d1c8b8
Tim Morgan <tim@timmorgan.org>
2015-08-27 20:28:53 -0500

    Fix spec

--- spec/models/import_attribute_spec.rb
+++ spec/models/import_attribute_spec.rb
@@ -2,7 +2,7 @@ require_relative '../rails_helper'
 
 describe ImportAttribute do
   describe 'validations' do
-    it { should validate_presence_of(:import) }
+    it { should validate_presence_of(:import_id) }
     it { should validate_presence_of(:row) }
   end
 end
