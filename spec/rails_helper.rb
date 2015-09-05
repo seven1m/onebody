279b42ec444b919923e7c7114a808964fba17c73
Tim Morgan <tim@timmorgan.org>
2015-09-05 10:48:52 -0500

    Set geocoding stub before each spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,17 @@ RSpec.configure do |config|
   config.before(:each) do
     ActionMailer::Base.deliveries.clear
     allow(StreamItemGroupJob).to receive(:perform_later)
+    Geocoder.configure(lookup: :test)
+    Geocoder::Lookup::Test.set_default_stub([{
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US',
+      'precision' => 'RANGE_INTERPOLATED'
+    }])
   end
 
   config.after(:each) do
