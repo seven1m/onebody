03b871306703ea045daefcbd5287be5e99b602d9
Tim Morgan <tim@timmorgan.org>
2015-09-05 09:06:48 -0500

    Remove spec for import attribute model

--- spec/models/import_attribute_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../rails_helper'
-
-describe ImportAttribute do
-  describe 'validations' do
-    it { should validate_presence_of(:import_id) }
-    it { should validate_presence_of(:row) }
-  end
-end
