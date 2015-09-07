2f3d255391bb4f27052beb7bae46db92ff660d06
Tim Morgan <tim@timmorgan.org>
2015-09-07 13:14:00 -0500

    Add site_id to document_folder_groups

--- app/models/document_folder_group.rb
+++ app/models/document_folder_group.rb
@@ -1,4 +1,5 @@
 class DocumentFolderGroup < ActiveRecord::Base
   belongs_to :document_folder
   belongs_to :group
+  scope_by_site_id
 end
---
+++ db/migrate/20150907165345_add_site_id_to_document_folder_groups.rb
@@ -0,0 +1,14 @@
+class AddSiteIdToDocumentFolderGroups < ActiveRecord::Migration
+  def up
+    add_column :document_folder_groups, :site_id, :integer
+    Site.each do
+      DocumentFolderGroup.connection.execute(
+        'update document_folder_groups set site_id = (select site_id from groups where id = document_folder_groups.id);'
+      )
+    end
+  end
+
+  def down
+    remove_column :document_folder_groups, :site_id
+  end
+end
