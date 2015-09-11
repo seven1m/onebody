db7bf244727a6098b6feca82435a2fc1fe34907e
Tim Morgan <tim@timmorgan.org>
2015-09-10 19:31:48 -0500

    Improve the Settings page

--- app/helpers/administration/settings_helper.rb
+++ app/helpers/administration/settings_helper.rb
@@ -1,33 +1,31 @@
 module Administration::SettingsHelper
-
-  def section_tab(label, name, active=false)
+  def section_tab(label, name, active = false)
     content_tag(:li, class: active ? 'active' : nil) do
       link_to label, "##{name}", data: { toggle: 'tab' }
     end
   end
 
-  def setting_row(section, name, options={}, &block)
-    if @setting = @settings[section][name]
-      content_tag(:div, class: "form-group setting#{@setting.id}") do
-        label_tag(@setting.id, setting_name(@setting), class: 'col-sm-2 control-label') +
+  def setting_row(section, name, options = {}, &block)
+    return unless (@setting = @settings[section][name])
+    content_tag(:div, class: "form-group setting#{@setting.id}") do
+      label_tag(@setting.id, setting_name(@setting), class: 'col-sm-2 control-label') +
         content_tag(:div, class: 'col-sm-10') do
           if block_given?
             capture(&block)
           else
             setting_field(options)
           end +
-          setting_description(@setting)
+            options[:description] || setting_description(@setting)
         end
-      end
     end
   end
 
-  def setting_field(options={})
+  def setting_field(options = {})
     @setting = @settings[options[:section]][options[:name]] if options[:section] and options[:name]
     if @setting.format == 'boolean'
       content_tag(:div, class: 'checkbox') do
-        hidden_field_tag(@setting.id, false, id: '') + \
-        check_box_tag(@setting.id, true, @setting.value?)
+        hidden_field_tag(@setting.id, false, id: '') +
+          check_box_tag(@setting.id, true, @setting.value?)
       end
     elsif @setting.format == 'list'
       text_area_tag(@setting.id, Array(@setting.value).join("\n"), rows: 3, cols: 40, class: 'form-control')
@@ -44,8 +42,8 @@ module Administration::SettingsHelper
 
   def setting_description(setting)
     content_tag(:span, class: 'help-block') do
-      I18n.t('description', scope: ['admin.settings', setting.section, setting.name], default: '')
+      scope = ['admin.settings', setting.section, setting.name]
+      I18n.t('description', scope: scope, default: '')
     end
   end
-  
 end
