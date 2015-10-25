407f1a58c3cbdc605520c9cfc1f4d8aaddc906f9
Tim Morgan <tim@timmorgan.org>
2015-10-24 22:44:29 -0500

    Revert "Update provision script to use debian package"
    This reverts commit 20b7a2f9348ae2bd7b0128858ca54ee16dff4eb1.

--- build/ubuntu/14.04/provision.sh
+++ build/ubuntu/14.04/provision.sh
@@ -3,19 +3,55 @@
 set -e
 
 sleep 10
-
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install -y curl
-curl https://apt.church.io/gpg.key | sudo apt-key add -
+sudo -E apt-get install -y -q wget build-essential libcurl4-openssl-dev libmysqlclient-dev nodejs git imagemagick mysql-server apache2 libapache2-mod-xsendfile
 
-echo "deb [arch=amd64] http://apt.church.io/ubuntu/nightly trusty main" | sudo tee /etc/apt/sources.list.d/onebody.list
-
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install -y onebody nginx
+sudo apt-get install -y ruby2.1 ruby2.1-dev
+sudo gem install bundler --no-rdoc --no-ri
 
-mysql -uroot -e "grant all on onebody.* to onebody@localhost identified by 'onebody';"
-sudo onebody run rake db:setup
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+echo "deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main" | sudo tee /etc/apt/sources.list.d/passenger.list
+sudo chmod 600 /etc/apt/sources.list.d/passenger.list
+sudo apt-get update
+sudo apt-get install -y libapache2-mod-passenger
+sudo a2enmod passenger
+sudo a2enmod xsendfile
+sudo sed -i "s/DocumentRoot.*/DocumentRoot \/var\/www\/onebody\/public\n\nXSendFile On\nXSendFilePath \/var\/www\/onebody\/public\/system/" /etc/apache2/sites-available/000-default.conf
+sudo service apache2 restart
+
+if [[ `grep RAILS_ENV .bashrc` == "" ]]; then
+  echo "export RAILS_ENV=production" | sudo tee -a $HOME/.bashrc
+fi
+
+cd /var/www
+[[ ! -e onebody ]] && sudo git clone git://github.com/churchio/onebody.git
+sudo chown -R $USER /var/www/onebody
+
+cd /var/www/onebody
+git checkout stable
+
+# brightbox may have a slightly newer version than we're pinned to -- that's ok
+sed -i '/ruby-version/d' Gemfile
+
+mysql -uroot -e "create database if not exists onebody default character set utf8 default collate utf8_general_ci; grant all on onebody.* to onebody@localhost identified by 'onebody';"
+if [[ -e config/database.yml.mysql-example ]]; then
+  cp config/database.yml{.mysql-example,}
+else
+  cp config/database.yml{.example,}
+fi
+
+bundle install
+
+RAILS_ENV=production bundle exec rake db:migrate db:seed
+
+cp config/secrets.yml{.example,}
+secret=`bundle exec rake secret`
+sed -i "s/SOMETHING_RANDOM_HERE/$secret/" config/secrets.yml
 
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y -q postfix courier-pop
@@ -33,6 +69,9 @@ if [[ ! -e /home/onebodymail ]]; then
   echo -e "production:\n  pop:\n    host: localhost\n    username: onebodymail\n    password: $email_password\n  smtp:\n    address: localhost\n    domain: example.com\n    enable_starttls_auto: false" > config/email.yml
 fi
 
-sudo onebody run whenever -w
+bundle exec whenever -w
+
+RAILS_ENV=production bundle exec rake tmp:clear assets:precompile
+touch tmp/restart.txt
 
 sudo apt-get clean
