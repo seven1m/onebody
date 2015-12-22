b657feea93f62faac786992b054c960a441faee1
Parker McGee <pmcgee@salesforce.com>
2015-12-22 16:21:05 -0500

    Make the giving interface nicer

--- app/models/donation_transaction.rb
+++ app/models/donation_transaction.rb
@@ -1,2 +1,9 @@
 class DonationTransaction < ActiveRecord::Base
+  def amount_as_currency
+    "$ #{amount.to_f / 100}"
+  end
+
+  def formatted_created_at
+    created_at.strftime('%B %d, %Y')
+  end
 end
