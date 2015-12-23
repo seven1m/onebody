f6de5d0e73f90185ccc74f057f4353d9a11209cd
Parker McGee <pmcgee@salesforce.com>
2015-12-23 11:49:21 -0500

    Sort transactions by created_at desc

--- app/controllers/giving/dashboard_controller.rb
+++ app/controllers/giving/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Giving::DashboardController < ApplicationController
   def show
-    @transactions = DonationTransaction.where(user_id: @logged_in.id)
+    @transactions = DonationTransaction.where(user_id: @logged_in.id).order(created_at: :desc)
   end
 end
