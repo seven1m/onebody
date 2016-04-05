ac38252633d4b3b0f88d2c65dd72bc9bc1112c9f
Tim Morgan <tim@timmorgan.org>
2016-04-04 21:01:42 -0500

    Update code style

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,12 +1,12 @@
 class Sanitize
   module Config
     ONEBODY = {
-      :elements => %w(
+      elements: %w(
         a b blockquote br caption cite code col colgroup dd div dl dt em h1 h2 h3 h4 h5 h6
         i img li ol p pre q small strike strong sub sup table tbody td tfoot th thead tr u ul
       ),
 
-      :attributes => {
+      attributes: {
         :all         => %w(style                                ),
         'a'          => %w(href title                           ),
         'blockquote' => %w(cite                                 ),
@@ -22,14 +22,14 @@ class Sanitize
         'ul'         => %w(type                                 )
       },
 
-      :protocols => {
-        'a'          => {'href' => ['ftp', 'http', 'https', 'mailto', :relative]},
-        'blockquote' => {'cite' => ['http', 'https', :relative                 ]},
-        'img'        => {'src'  => ['http', 'https', :relative                 ]},
-        'q'          => {'cite' => ['http', 'https', :relative                 ]}
+      protocols: {
+        'a'          => { 'href' => ['ftp', 'http', 'https', 'mailto', :relative] },
+        'blockquote' => { 'cite' => ['http', 'https', :relative] },
+        'img'        => { 'src'  => ['http', 'https', :relative] },
+        'q'          => { 'cite' => ['http', 'https', :relative] }
       },
 
-      :remove_contents => %w(script style)
-    }
+      remove_contents: %w(script style)
+    }.freeze
   end
 end
