7e75ad251bb1fae2ec519906b4bcc079295de4b9
Tim Morgan <tim@timmorgan.org>
2016-04-04 21:29:23 -0500

    Allow span tags in HTML emails

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -3,7 +3,7 @@ class Sanitize
     ONEBODY = {
       elements: %w(
         a b blockquote br caption cite code col colgroup dd div dl dt em h1 h2 h3 h4 h5 h6
-        i img li ol p pre q small strike strong sub sup table tbody td tfoot th thead tr u ul
+        i img li ol p pre q small span strike strong sub sup table tbody td tfoot th thead tr u ul
       ),
 
       attributes: {
