65046b9a7b2891e0bebbab0b7fb5731f0430780e
Tim Morgan <tim@timmorgan.org>
2016-04-05 20:29:10 -0500

    Update code style

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -12,19 +12,29 @@ describe Message do
     @group_admin.memberships.create!(group: @group, admin: true)
   end
 
-  it "should create a new message with attachments" do
-    files = [Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/attachment.pdf'), 'application/pdf', true)]
-    @message = Message.create_with_attachments({to: @person, person: @second_person, subject: 'subject', body: 'body'}, files)
+  it 'creates a new message with attachments' do
+    files = [
+      Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/attachment.pdf'), 'application/pdf', true)
+    ]
+    @message = Message.create_with_attachments(
+      {
+        to: @person,
+        person: @second_person,
+        subject: 'subject',
+        body: 'body'
+      },
+      files
+    )
     expect(@message.attachments.count).to eq(1)
   end
 
-  it "should preview a message" do
+  it 'previews a message' do
     @preview = Message.preview(to: @person, person: @second_person, subject: 'subject', body: 'body')
-    expect(@preview.subject).to eq("subject")
+    expect(@preview.subject).to eq('subject')
     @body = get_email_body(@preview)
     expect(@body.to_s.index('body')).to be
     expect(@body.to_s).to match(/Hit "Reply" to send a message/)
-    expect(@body.to_s).to match(/http:\/\/.+\/privacy/)
+    expect(@body.to_s).to match(%r{http://.+\/privacy})
   end
 
   describe '#can_read?' do
@@ -35,8 +45,8 @@ describe Message do
 
       it 'knows who can see the message' do
         expect(@person.can_read?(@message)).to eq(true)
-        expect(!!@second_person.can_read?(@message)).to eq(false)
-        expect(!!@admin_person.can_read?(@message)).to eq(false)
+        expect(@second_person.can_read?(@message)).to be_falsey
+        expect(@admin_person.can_read?(@message)).to be_falsey
         expect(@group_admin.can_read?(@message)).to eq(true)
       end
 
@@ -47,8 +57,8 @@ describe Message do
 
         it 'knows who can see the message' do
           expect(@person.can_read?(@message)).to eq(true)
-          expect(!!@second_person.can_read?(@message)).to eq(false)
-          expect(!!@admin_person.can_read?(@message)).to eq(false)
+          expect(@second_person.can_read?(@message)).to be_falsey
+          expect(@admin_person.can_read?(@message)).to be_falsey
           expect(@group_admin.can_read?(@message)).to eq(true)
         end
 
@@ -58,7 +68,7 @@ describe Message do
           end
 
           it 'does not allow admin to see' do
-            expect(!!@admin_person.can_read?(@message)).to eq(false)
+            expect(@admin_person.can_read?(@message)).to be_falsey
           end
         end
       end
@@ -77,19 +87,19 @@ describe Message do
     end
   end
 
-  it 'should allow a message without body if it has an html body' do
+  it 'allows a message without body if it has an html body' do
     @message = Message.create(subject: 'foo', html_body: 'bar', person: @person, group: @group)
     expect(@message).to be_valid
   end
 
-  it 'should be invalid if no body or html body' do
+  it 'is invalid if there is no body or html body' do
     @message = Message.create(subject: 'foo', person: @person, group: @group)
     expect(@message).to_not be_valid
     expect(@message.errors[:body]).to be_any
   end
 
-  it 'should not allow two identical messages to be saved' do
-    details = {subject: 'foo', body: 'foo', person: @person, group: @group}
+  it 'does not allow two identical messages to be saved' do
+    details = { subject: 'foo', body: 'foo', person: @person, group: @group }
     @message = Message.create!(details)
     @message2 = Message.new(details)
     expect(@message2).to_not be_valid
@@ -190,7 +200,7 @@ describe Message do
 
       it 'includes the link to disable group email' do
         expect(instructions).to match(
-          %r{\ATo stop email from this group:\n.*email=off\n\z}
+          /\ATo stop email from this group:\n.*email=off\n\z/
         )
       end
     end
@@ -204,7 +214,7 @@ describe Message do
 
       it 'includes the link to the privacy page' do
         expect(instructions).to match(
-          %r{\ATo stop these emails, go to your privacy page:\n.*privacy\n\z}
+          /\ATo stop these emails, go to your privacy page:\n.*privacy\n\z/
         )
       end
     end
