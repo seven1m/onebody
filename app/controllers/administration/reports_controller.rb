d8483dea41e72d37f2be444da25c5f5a257e7169
Tim Morgan <tim@timmorgan.org>
2017-02-07 19:30:52 -0600

    Remove other custom report stuff

--- app/controllers/administration/reports_controller.rb
+++ app/controllers/administration/reports_controller.rb
@@ -1,8 +1,5 @@
 class Administration::ReportsController < ApplicationController
-
   def index
     @reports = I18n.t('reports.reports')
-    @custom_reports = CustomReport.order(:title)
   end
-
 end
