1a8335878ba69707231aecfb3107e8b8979a2e4f
Tim Morgan <tim@timmorgan.org>
2017-02-24 20:27:31 -0600

    Rewrite printed directory

--- app/models/concerns/person/pdf_gen.rb
+++ app/models/concerns/person/pdf_gen.rb
@@ -1,122 +1,15 @@
 module Concerns
   module Person
     module PdfGen
-      def generate_directory_pdf(with_pictures=false)
-        pdf = PDF::Writer.new
-        pdf.margins_pt 70, 20, 20, 20
-        pdf.open_object do |heading|
-          pdf.save_state
-          pdf.stroke_color! Color::RGB::Black
-          pdf.stroke_style! PDF::Writer::StrokeStyle::DEFAULT
-
-          size = 24
-
-          x = pdf.absolute_left_margin
-          y = pdf.absolute_top_margin + 30
-          pdf.add_text x, y, "#{Setting.get(:name, :community)} #{I18n.t('nav.directory')}\n\n", size
-
-          x = pdf.absolute_left_margin
-          w = pdf.absolute_right_margin
-          #y -= (pdf.font_height(size) * 1.01)
-          y -= 10
-          pdf.line(x, y, w, y).stroke
-
-          pdf.restore_state
-          pdf.close_object
-          pdf.add_object(heading, :all_following_pages)
-        end
-
-        s = 24
-        w = pdf.text_width(Setting.get(:name, :community), s)
-        x = pdf.margin_x_middle - w/2 # centered
-        y =  pdf.absolute_top_margin - 150
-        pdf.add_text x, y, Setting.get(:name, :community), s
-        s = 20
-        w = pdf.text_width('Directory', s)
-        x = pdf.margin_x_middle - w/2 # centered
-        y =  pdf.absolute_top_margin - 200
-        pdf.add_text x, y, I18n.t('nav.directory'), s
-
-        t = I18n.t('printable_directories.created_for', { name: self.name, date: (Date.today.strftime '%B %e, %Y') } )
-        s = 14
-        w = pdf.text_width(t, s)
-        x = pdf.margin_x_middle - w/2 # centered
-        y = pdf.margin_y_middle - pdf.margin_height/3 # below center
-        pdf.add_text x, y, t, s
-
-        pdf.start_new_page
-        pdf.start_columns
-
-        alpha = nil
-
-        families = Family.undeleted.has_printable_people.includes(:people) \
-          .order('families.last_name, families.name, people.position').references(:people)
-        families.each do |family|
-          if family.mapable? or family.home_phone.to_i > 0
-            pdf.move_pointer 120 if pdf.y < 120
-            if family.last_name[0..0] != alpha
-              if with_pictures and family.photo.exists?
-                pdf.move_pointer 450 if pdf.y < 450
-              else
-                pdf.move_pointer 150 if pdf.y < 150
-              end
-              alpha = family.last_name[0..0]
-              pdf.text alpha + "\n", font_size: 25
-              pdf.line(
-                pdf.absolute_left_margin,
-                pdf.y - 5,
-                pdf.absolute_left_margin + pdf.column_width - 25,
-                pdf.y - 5
-              ).stroke
-              pdf.move_pointer 10
-            end
-            if with_pictures and family.photo.exists?
-              if pdf.y < 300
-                pdf.move_pointer 300
-              else
-                pdf.move_pointer 20
-              end
-              pdf.text family.name + "\n", font_size: 18
-              pdf.move_pointer 10
-              image = MiniMagick::Image.new(family.photo.path(:large))
-              width = (150.0 / image[:height] * image[:width]).to_i
-              image.format('jpg') if image.type == 'PNG'
-              pdf.add_image image.to_blob, pdf.absolute_left_margin, pdf.y-150, width, 150
-              pdf.move_pointer 160
-            else
-              pdf.text family.name + "\n", font_size: 18
-            end
-            if family.people.detect { |p| p.share_address_with(self) } and [family.address1, family.city, family.state].all?(&:present?)
-              pdf.text family.address1 + "\n", font_size: 14
-              pdf.text family.address2 + "\n" if family.address2.present?
-              pdf.text family.city + ', ' + family.state + '  ' + family.zip.to_s + "\n"
-            end
-            pdf.text ApplicationHelper.format_phone(family.home_phone), font_size: 14 if family.home_phone.to_i > 0
-            family.people.undeleted.each do |person|
-              name = person.last_name == family.last_name ? person.first_name : person.name
-              pdf.text "\n", font_size: 11
-              pdf.add_text_wrap pdf.absolute_left_margin, pdf.y, 400, name, 11
-              details = ''
-              if person.show_attribute_to?(:mobile_phone, self)
-                details += '   ' + ApplicationHelper.format_phone(person.mobile_phone, :mobile)
-              end
-              if person.show_attribute_to?(:email, self)
-                details += '   ' + person.email
-              end
-              pdf.add_text_wrap pdf.absolute_left_margin + pdf.text_width(name, 11), pdf.y, 200, details, 9
-            end
-            pdf.text "\n"
-          end
-        end
-
-        pdf
+      def generate_directory_pdf(with_pictures = false)
+        PrintableDirectory.new(self, pictures: with_pictures).render
       end
 
-      def generate_directory_pdf_to_file(filename, with_pictures=false)
+      def generate_directory_pdf_to_file(filename, with_pictures = false)
         File.open(filename, 'wb') { |f| f.write(generate_directory_pdf(with_pictures)) }
       end
 
-      def generate_and_email_directory_pdf(with_pictures=false)
+      def generate_and_email_directory_pdf(with_pictures = false)
         filename = "#{Rails.root}/tmp/directory_for_user#{id}.pdf"
         generate_directory_pdf_to_file(filename, with_pictures)
         Notifier.printed_directory(self, File.open(filename)).deliver_now
---
+++ app/models/printable_directory.rb
@@ -0,0 +1,200 @@
+class PrintableDirectory
+  def initialize(person, pictures: false)
+    @person = person
+    @pictures = pictures
+    @pdf = Prawn::Document.new
+  end
+
+  attr_reader :person, :pictures
+
+  delegate \
+    :bounding_box,
+    :bounds,
+    :column_box,
+    :cursor,
+    :image,
+    :move_down,
+    :repeat,
+    :start_new_page,
+    :stroke_horizontal_rule,
+    :text,
+    to: :@pdf
+
+  def render
+    set_font
+    render_cover_page
+    start_new_page
+    render_header
+    render_families
+    @pdf.render
+  end
+
+  private
+
+  def set_font
+    @pdf.font Rails.root.join('vendor/assets/fonts/OpenSans-Regular.ttf')
+    @pdf.default_leading 3
+  end
+
+  def render_cover_page
+    bounding_box [bounds.left, bounds.top / 2 + 100], width: bounds.width do
+      text Setting.get(:name, :community),
+           align: :center,
+           size: 30
+      text I18n.t('printable_directories.pdf.cover_page_sub_heading'),
+           align: :center,
+           size: 25
+      move_down 200
+      text I18n.t('printable_directories.pdf.created_for', name: person.name, date: Date.current.to_s(:date)),
+           align: :center,
+           size: 10
+    end
+  end
+
+  def render_header
+    repeat(->(pg) { pg > 1 }, dynamic: true) do
+      bounding_box [bounds.left, bounds.top], width: bounds.width do
+        text I18n.t('printable_directories.pdf.heading', community: Setting.get(:name, :community)),
+             align: :center,
+             size: 18
+        stroke_horizontal_rule
+      end
+    end
+  end
+
+  FAMILY_MARGIN = 15
+
+  def render_families
+    @last_alpha = nil
+    column_box [bounds.left, bounds.top - 50], width: bounds.width, height: bounds.height - 50, columns: 2 do
+      families.each do |family|
+        alpha = family.last_name[0].upcase
+        photo = @pictures && family_photo(family)
+        move_down cursor unless alpha_and_family_fits?(alpha, family, photo)
+        if (alpha_heading = formatted_alpha_heading(alpha))
+          text alpha_heading, inline_format: true
+          stroke_horizontal_rule
+          text "\n"
+        end
+        render_photo(photo) if @pictures
+        text formatted_family(family), inline_format: true
+        move_down FAMILY_MARGIN
+        @last_alpha = alpha
+      end
+    end
+  end
+
+  PHOTO_MARGIN = 15
+
+  def render_photo(photo)
+    return unless photo
+    image photo.path, width: image_bounds_width
+    move_down PHOTO_MARGIN
+  end
+
+  def alpha_and_family_fits?(alpha, family, photo)
+    trial = Prawn::Text::Formatted::Box.new(
+      [{ text: formatted_alpha_and_family(alpha, family) }],
+      at: [bounds.left, bounds.top],
+      width: bounds.width,
+      inline_format: true,
+      document: @pdf
+    )
+    trial.render(dry_run: true)
+    height = trial.height
+    height += family_photo_height(photo) + PHOTO_MARGIN if photo
+    cursor - height >= bounds.bottom
+  end
+
+  def formatted_alpha_and_family(alpha, family)
+    [
+      formatted_alpha_heading(alpha),
+      formatted_family(family)
+    ].compact.join("\n")
+  end
+
+  def formatted_alpha_heading(alpha)
+    return if alpha == @last_alpha
+    "<font size='20'>#{alpha}</font>\n"
+  end
+
+  def formatted_family(family)
+    [
+      "<font size='18'>#{h family.name}</font>",
+      formatted_address(family),
+      formatted_home_phone(family),
+      family.people.undeleted.map do |p|
+        [
+          formatted_name(p, family),
+          formatted_mobile_phone(p),
+          formatted_email(p)
+        ].compact.join(' · ')
+      end
+    ].flatten.compact.join("\n")
+  end
+
+  def formatted_address(family)
+    return if family.pretty_address.blank?
+    return unless family.people.undeleted.detect { |p| p.share_address_with(person) }
+    "<font size='14'>#{h family.pretty_address.strip}</font>"
+  end
+
+  def formatted_home_phone(family)
+    return if family.home_phone.blank?
+    "<font size='14'>#{h format_phone(family.home_phone)}</font>"
+  end
+
+  def formatted_name(p, family)
+    name = if p.last_name == family.last_name
+             p.first_name
+           else
+             p.name
+           end
+    "<font size='12'>#{h name}</font>"
+  end
+
+  def formatted_mobile_phone(p)
+    return unless p.show_attribute_to?(:mobile_phone, person)
+    "<font size='10'>#{h format_phone(p.mobile_phone)}</font>"
+  end
+
+  def formatted_email(p)
+    return unless p.show_attribute_to?(:email, person)
+    "<font size='10'>#{h p.email}</font>"
+  end
+
+  def family_photo(family)
+    path = if family.photo.present?
+              family.photo.path(:large)
+            elsif family.people.undeleted.size == 1
+              family.people.undeleted.first.photo.presence.try(:path, :large)
+            end
+    return unless path && File.exist?(path)
+    MiniMagick::Image.new(path)
+  end
+
+  def family_photo_height(photo)
+    #(image_bounds_width / photo[:height] * photo[:width]).to_i
+    (photo[:height] * image_bounds_width / photo[:width]).to_i
+  end
+
+  def image_bounds_width
+    bounds.width * 0.6
+  end
+
+  def families
+    Family.undeleted
+          .has_printable_people
+          .order('families.last_name, families.name, people.position')
+          .includes(:people)
+          .references(:people)
+  end
+
+  def format_phone(phone)
+    ApplicationHelper.format_phone(phone)
+  end
+
+  def h(html)
+    ERB::Util.h(html)
+  end
+end
