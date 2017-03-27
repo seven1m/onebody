bf4f833194710a563d4946e1897c1db5f0cab7a7
Tim Morgan <tim@timmorgan.org>
2017-03-26 22:24:20 -0500

    Add some more to the EmailSetup process

--- app/models/email_setup.rb
+++ app/models/email_setup.rb
@@ -3,7 +3,35 @@ class EmailSetup
     @key = key
   end
 
-  def go
-    # do stuff
+  def save!
+    create_catch_all
+    write_config
+  end
+
+  private
+
+  def api
+    @api ||= MailgunApi.new(@key)
+  end
+
+  delegate :create_catch_all, to: :api
+
+  def write_config
+    # TODO
+  end
+
+  def config
+    {
+      Rails.env.to_s => {
+        'smtp' => {
+          'address'        => 'smtp.mailgun.org',
+          'port'           => 587,
+          'domain'         => Site.current.email_host,
+          'authentication' => 'plain',
+          'user_name'      => 'TODO',
+          'password'       => 'TODO'
+        }
+      }
+    }
   end
 end
