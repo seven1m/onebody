a2bcca4b143179e000a26421981803f11a285907
Tim Morgan <tim@timmorgan.org>
2017-05-02 22:03:15 -0700

    rename 'pt' translation to 'pt-BR'

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,6 +1,6 @@
 TranslationIO.configure do |config|
   config.api_key        = ENV['TRANSLATION_IO_API_KEY']
   config.source_locale  = 'en'
-  config.target_locales = ['af-ZA', 'da', 'de', 'es-MX', 'fr', 'nl-NL', 'pt', 'ro', 'ru']
+  config.target_locales = ['af-ZA', 'da', 'de', 'es-MX', 'fr', 'nl-NL', 'pt-BR', 'ro', 'ru']
   config.disable_gettext = true
 end
