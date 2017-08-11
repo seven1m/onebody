ed7fddd19b369117a2492f470879c65d23a3c205
Tim Morgan <tim@timmorgan.org>
2017-08-11 10:23:24 -0500

    Add custom select fields to profile edit UI

--- app/helpers/custom_fields_helper.rb
+++ app/helpers/custom_fields_helper.rb
@@ -2,4 +2,11 @@ module CustomFieldsHelper
   def show_custom_fields?
     CustomField.any?
   end
+
+  def custom_field_select_options(field)
+    options_for_select(
+      [nil] + field.options.map { |o| [o.label, o.id] },
+      @person && @person.fields[field.id]
+    )
+  end
 end
--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -7,6 +7,8 @@ class CustomField < ActiveRecord::Base
   has_many :custom_field_values, foreign_key: 'field_id', dependent: :delete_all
   has_many :custom_field_options, foreign_key: 'field_id', dependent: :delete_all
 
+  alias options custom_field_options
+
   accepts_nested_attributes_for :custom_field_options, allow_destroy: true
 
   def slug
