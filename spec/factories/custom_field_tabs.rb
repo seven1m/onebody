804b6c3287394ccdeb25d69c0feb4b66da0a00c2
Tim Morgan <tim@timmorgan.org>
2017-08-19 15:28:17 -0500

    Fix specs

---
+++ spec/factories/custom_field_tabs.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :custom_field_tab do
+    name 'Tab'
+  end
+end
--- spec/factories/custom_fields.rb
+++ spec/factories/custom_fields.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :custom_field do
+    association :tab, factory: :custom_field_tab
     name 'Age Group'
     format 'string'
   end
