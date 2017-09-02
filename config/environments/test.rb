2c247042fa8eb483c1d525670533478d3efb2672
Tim Morgan <tim@timmorgan.org>
2017-09-02 11:19:11 -0500

    Use updated config option

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,7 @@ OneBody::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files   = true
+  config.public_file_server.enabled = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching.
