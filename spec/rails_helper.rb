226d131f1d8973e587b4a13e8a6c0980276ea8d8
Tim Morgan <tim@timmorgan.org>
2017-09-14 22:40:31 -0500

    Formatting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,15 +40,15 @@ RSpec.configure do |config|
     allow(StreamItemGroupJob).to receive(:perform_later)
     Geocoder.configure(lookup: :test)
     Geocoder::Lookup::Test.set_default_stub([{
-                                              'latitude'     => 40.7143528,
-                                              'longitude'    => -74.0059731,
-                                              'address'      => 'New York, NY, USA',
-                                              'state'        => 'New York',
-                                              'state_code'   => 'NY',
-                                              'country'      => 'United States',
-                                              'country_code' => 'US',
-                                              'precision'    => 'RANGE_INTERPOLATED'
-                                            }])
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US',
+      'precision'    => 'RANGE_INTERPOLATED'
+    }])
   end
 
   config.after(:each) do
