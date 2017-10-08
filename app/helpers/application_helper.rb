97b58d4b5f7fbcac6c9a25d3ee6e810c0c8c783b
fadiwissa <fadiwissa@gmail.com>
2017-10-08 18:34:30 +0200

    Fixing date_format null exception

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -151,7 +151,9 @@ module ApplicationHelper
   end
 
   def datepicker_format
-    date_format.downcase
+    if (date_format)
+      date_format.downcase
+    date_format
   end
 
   # TODO: replace all inline JS links with unobtrusive JS
